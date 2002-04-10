e3e3e4e566549d6f336fb77ee487e5c7015904be
Stephen Weeks <sweeks@sweeks.com>
2002-04-10 07:17:33 +0000

    Fixing broken comment.

--- include/mlton.h
+++ include/mlton.h
@@ -1,4 +1,4 @@
-(* Copyright (C) 1999-2002 Henry Cejtin, Matthew Fluet, Suresh
+/* Copyright (C) 1999-2002 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  * Copyright (C) 1997-1999 NEC Research Institute.
  *
