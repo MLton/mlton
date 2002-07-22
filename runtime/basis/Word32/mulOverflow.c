08ba70073e4bc536af67065ac5d1301f388d29b4
Stephen Weeks <sweeks@sweeks.com>
2002-07-22 02:34:02 +0000

    Adding casts to make printf happy.

--- runtime/basis/Word32/mulOverflow.c
+++ runtime/basis/Word32/mulOverflow.c
@@ -9,6 +9,6 @@ Word Word32_mulOverflow(Word lhs, Word rhs, Bool *overflow) {
 	*overflow = (tmp != (Word)tmp);
 	if (FALSE)
 		fprintf (stderr, "0x%x * 0x%x = 0x%llu (overflow = %d)\n",
-				lhs, rhs, tmp, *overflow);
+				(uint)lhs, (uint)rhs, tmp, *overflow);
 	return tmp;
 }
