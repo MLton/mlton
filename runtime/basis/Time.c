9b250e69a1a0058136c9f0c5325a97dc96b345f6
Stephen Weeks <sweeks@sweeks.com>
2002-09-19 06:51:43 +0000

    formatting

--- runtime/basis/Time.c
+++ runtime/basis/Time.c
@@ -4,14 +4,14 @@
 
 static struct timeval timeval;
 
-Int Time_gettimeofday() {
-	return gettimeofday(&timeval, (struct timezone*)NULL);
+Int Time_gettimeofday () {
+	return gettimeofday (&timeval, (struct timezone*)NULL);
 }
 
-Int Time_sec() {
+Int Time_sec () {
 	return timeval.tv_sec;
 }
 
-Int Time_usec() {
+Int Time_usec () {
 	return timeval.tv_usec;
 }
