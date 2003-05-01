d4dd47404a70919ee9b94bebfb1e4dc01448d297
Stephen Weeks <sweeks@sweeks.com>
2003-05-01 22:35:33 +0000

    Use O_LARGEFILE to open files.

--- runtime/Posix/FileSys/open.c
+++ runtime/Posix/FileSys/open.c
@@ -3,11 +3,16 @@
 #include <fcntl.h>
 #include "mlton-posix.h"
 
+/* FreeBSD uses 64 bits files by default, so doesn't have O_LARGEFILE. */
+#if (defined __FreeBSD__)
+#define O_LARGEFILE 0
+#endif
+
 Int Posix_FileSys_open (NullString p, Word w, Mode m) {
 
 #if (defined (__linux__) || defined (__FreeBSD__) || defined (__sun__))
 
-	return open ((char *) p, w, m);
+	return open ((char *) p, w | O_LARGEFILE, m);
 
 #elif (defined (__CYGWIN__))
 
@@ -16,7 +21,7 @@ Int Posix_FileSys_open (NullString p, Word w, Mode m) {
 	stat ((char *) p, &buf);
 	if (S_ISDIR (buf.st_mode))
 		return -1;
-	return open((char *) p, w, m);
+	return open ((char *) p, w, m);
 
 #else
 
