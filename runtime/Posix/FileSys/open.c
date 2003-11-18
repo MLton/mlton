5e903cbe444508a5ccd1d9cd6762156f7048c04e
Stephen Weeks <sweeks@sweeks.com>
2003-11-18 22:33:33 +0000

    Added debugging messages.

--- runtime/Posix/FileSys/open.c
+++ runtime/Posix/FileSys/open.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "mlton-posix.h"
+
+enum {
+	DEBUG = 0,
+};
 
 /* {Free,Net}BSD use 64 bits files by default, so doesn't have O_LARGEFILE. */
 #if (defined (__FreeBSD__) || defined (__NetBSD__))
@@ -9,10 +14,11 @@
 #endif
 
 Int Posix_FileSys_open (NullString p, Word w, Mode m) {
+	Int res;
 
 #if (defined (__FreeBSD__) || defined (__linux__) || defined (__NetBSD__) || defined (__sun__))
 
-	return open ((char *) p, w | O_LARGEFILE, m);
+	res = open ((char *) p, w | O_LARGEFILE, m);
 
 #elif (defined (__CYGWIN__))
 
@@ -20,12 +26,21 @@ Int Posix_FileSys_open (NullString p, Word w, Mode m) {
 
 	stat ((char *) p, &buf);
 	if (S_ISDIR (buf.st_mode))
-		return -1;
-	return open ((char *) p, w, m);
+		res = -1;
+	else
+		res = open ((char *) p, w, m);
 
 #else
 
 #error Posix_FileSys_open not defined
 
 #endif
+
+	if (DEBUG)
+		fprintf (stderr, "%d = Posix_FileSys_open (%s, 0x%08x, 0x%08x)\n", 
+				(int)res, 
+				(unsigned int)p, 
+				(unsigned int)w,
+				(unsigned int)m);
+	return res;
 }
