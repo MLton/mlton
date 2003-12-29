bf95d5c849fcedb630c7e80094fbea5409e51b00
Stephen Weeks <sweeks@sweeks.com>
2003-12-29 23:56:49 +0000

    Taking out the special case test for open() of a directory on Cygwin. It was mistaken because it didn't set errno.  Also, it's not entirely clear that we really want to handle it specially -- maybe it's better to just reflect the system call result.  Finally, if we do want to special case things, it would be better to do it in the basis library code.

--- runtime/Posix/FileSys/open.c
+++ runtime/Posix/FileSys/open.c
@@ -11,25 +11,7 @@ enum {
 Int Posix_FileSys_open (NullString p, Word w, Mode m) {
 	Int res;
 
-#if (defined (__FreeBSD__) || defined (__linux__) || defined (__NetBSD__) || defined (__sun__))
-
 	res = open ((char *) p, w, m);
-
-#elif (defined (__CYGWIN__))
-
-	struct stat buf;
-
-	stat ((char *) p, &buf);
-	if (S_ISDIR (buf.st_mode))
-		res = -1;
-	else
-		res = open ((char *) p, w, m);
-
-#else
-
-#error Posix_FileSys_open not defined
-
-#endif
 
 	if (DEBUG)
 		fprintf (stderr, "%d = Posix_FileSys_open (%s, 0x%08x, 0x%08x)\n", 
