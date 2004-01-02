97f8a5f6a103d6be787fca95f240dbe7b1d3772f
Stephen Weeks <sweeks@sweeks.com>
2004-01-02 03:57:20 +0000

    Making the default ramSlop be 0.5. Making the default ramSlop when mlton runs be 0.5.
    Both of those are done so that mlton does not act like a space pig by
    default.
    
    Making the ramSlop 0.8 when self compiling, because we usually need
    the memory, and because people are more likely to expect it to be a
    pig.

--- runtime/gc.c
+++ runtime/gc.c
@@ -4366,7 +4366,7 @@ int GC_init (GC_state s, int argc, char **argv) {
 	s->nurseryRatio = 10.0;
 	s->oldGenArraySize = 0x100000;
 	s->pageSize = getpagesize ();
-	s->ramSlop = 0.70;
+	s->ramSlop = 0.5;
 	s->savedThread = BOGUS_THREAD;
 	s->signalHandler = BOGUS_THREAD;
 	s->signalIsPending = FALSE;
