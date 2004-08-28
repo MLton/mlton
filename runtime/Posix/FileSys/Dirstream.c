afde1dc31c3ea886e2b407546a98f4b9835cc767
Stephen Weeks <sweeks@sweeks.com>
2004-08-28 18:59:29 +0000

    Add debugging messages.

--- runtime/Posix/FileSys/Dirstream.c
+++ runtime/Posix/FileSys/Dirstream.c
@@ -1,20 +1,43 @@
 #include "platform.h"
 
+enum {
+	DEBUG_DIRSTREAM = FALSE,
+};
+
 Int Posix_FileSys_Dirstream_closedir (Cpointer p) {
-	return (Int)(closedir ((DIR *) p));
+	Int res;
+
+	res = (Int)(closedir ((DIR *) p));
+	if (DEBUG_DIRSTREAM)
+		fprintf (stderr, "%d = closedir (0x%08x)\n", (uint)res, (uint)p);
+	return res;
 }
 
 Cpointer Posix_FileSys_Dirstream_opendir (Cpointer p) {
-	return (Cpointer)(opendir ((char *) p));
+	Cpointer res;
+
+	res = (Cpointer)(opendir ((char *) p));
+	if (DEBUG_DIRSTREAM)
+		fprintf (stderr, "0x%08x = opendir (%s)\n", 
+				(uint)res, (char *)p);
+	return res;
 }
 
 Cstring Posix_FileSys_Dirstream_readdir (Cpointer d) {
 	struct dirent *e;
+	Cstring res;
 	
 	e = readdir ((DIR *) d);
-	return (Cstring)((NULL == e) ? NULL : e->d_name);
+	res = (Cstring)((NULL == e) ? NULL : e->d_name);
+	if (DEBUG_DIRSTREAM)
+		fprintf (stderr, "%s = readdir (0x%08x)\n", 
+				((Cstring)NULL == res) ? "NULL": (char*)res,
+				(uint)d);
+	return res;
 }
 
 void Posix_FileSys_Dirstream_rewinddir (Cpointer p) {
-	rewinddir ((DIR *) p);
+	if (DEBUG_DIRSTREAM)
+		fprintf (stderr, "rewinddir (0x%08x)\n", (uint)p);
+       	rewinddir ((DIR *) p);
 }
--- runtime/Posix/IO/write.c
+++ runtime/Posix/IO/write.c
@@ -1,6 +1,15 @@
 #include "platform.h"
 
-Ssize Posix_IO_write (Fd fd, Pointer b, Int i, Size s) {
+enum {
+	DEBUG_WRITE = FALSE,
+};
 
-	return (Ssize)(write (fd, (void *) ((char *) b + i), s));
+Ssize Posix_IO_write (Fd fd, Pointer b, Int i, Size s) {
+	Ssize res;
+	
+	res = (Ssize)(write (fd, (void *) ((char *) b + i), s));
+	if (DEBUG_WRITE)
+		fprintf (stderr, "%d = Posix_IO_write (%d, 0x%08x, %d, %d)\n",
+				(int)res, (int)fd, (uint)b, (int)i, (int)s);
+	return res;
 }
