bc2d27dddfddb3e3fb40234e9d8d4f9299b1c561
Stephen Weeks <sweeks@sweeks.com>
2004-12-23 23:01:49 +0000

    Use the C99 signbit macro.

--- runtime/basis/Real/signBit.c
+++ runtime/basis/Real/signBit.c
@@ -1,15 +1,9 @@
 #include "platform.h"
 
 Int Real32_signBit (Real32 f) {
-	int offset;
-
-	offset = MLton_Platform_Arch_bigendian ? 0 : 3;
-	return (((unsigned char *)&f)[offset] & 0x80) >> 7;
+	return signbit (f);
 }
 
 Int Real64_signBit (Real64 d) {
-	int offset;
-
-	offset = MLton_Platform_Arch_bigendian ? 0 : 7;
-	return (((unsigned char *)&d)[offset] & 0x80) >> 7;
+	return signbit (d);
 }
