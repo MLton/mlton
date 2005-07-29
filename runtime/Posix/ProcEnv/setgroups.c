042ba3f9843ab9a3ba3dc92bce115c8be30e80ff
Stephen Weeks <sweeks@sweeks.com>
2005-07-29 04:22:34 +0000

    Don't use "for (int i ...", which fails with gcc 2.96.

--- runtime/Posix/ProcEnv/setgroups.c
+++ runtime/Posix/ProcEnv/setgroups.c
@@ -1,6 +1,7 @@
 #include "platform.h"
 
 Int Posix_ProcEnv_setgroups (Pointer groups) {
+	int i;
 	gid_t *list;
 	int res;
 	int size;
@@ -8,7 +9,7 @@ Int Posix_ProcEnv_setgroups (Pointer groups) {
 	size = GC_arrayNumElements (groups);
 	ARRAY (gid_t*, list, size);
 	assert (size <= cardof (list));
-	for (int i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 		list[i] = ((Word*)groups)[i];
 	res = setgroups (size, list);
 	free (list);
