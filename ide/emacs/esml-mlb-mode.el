3604bf4c0be9e619c4759b6e0d3d21dd35477e7f
Vesa Karvonen <vesa.a.j.k@gmail>
2005-08-13 23:06:16 +0000

    Switched to the MLton-LICENSE.
    SVN r3971

--- ide/emacs/esml-mlb-mode.el
+++ ide/emacs/esml-mlb-mode.el
@@ -1,23 +1,13 @@
-;; Emacs mode for editing ML Basis files.
-;; Copyright (C) 2005  Vesa Karvonen (vesa.karvonen@cs.helsinki.fi)
+;; Copyright (C) 2005 Vesa Karvonen
 ;;
-;; This program is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the
-;; Free Software Foundation; either version 2 of the License, or (at your
-;; option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License along
-;; with this program; if not, write to the Free Software Foundation, Inc.,
-;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+;; MLton is released under a BSD-style license.
+;; See the file MLton-LICENSE for details.
 
 (provide 'esml-mlb-mode)
 (require 'cl)
 
+;; Emacs mode for editing ML Basis files
+;;
 ;; Installation
 ;; ============
 ;;
