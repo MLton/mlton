81e528e4fe532b7bb5814e7434fbfe0f49dc2aae
Matthew Fluet <matthew.fluet@gmail.com>
2006-01-27 01:54:17 +0000

    Added assert on signedness of char
    SVN r4311

--- runtime/cenv.h
+++ runtime/cenv.h
@@ -52,6 +52,7 @@
 COMPILE_TIME_ASSERT(CHAR_BIT__is_eight, CHAR_BIT == 8);
 COMPILE_TIME_ASSERT(sizeof_float__is_four, sizeof(float) == 4);
 COMPILE_TIME_ASSERT(sizeof_double__is_eight, sizeof(double) == 8);
+COMPILE_TIME_ASSERT(char__is_signed, (double)((char)(-1)) < 0);
 
 #include <fcntl.h>
 #include <unistd.h>
