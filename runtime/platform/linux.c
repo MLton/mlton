ad4abf93a7250a25ca650cf03007d7ff4d5b9513
Matthew Fluet <matthew.fluet@gmail.com>
2006-05-08 02:01:49 +0000

    Added ((noreturn)) attribute
    SVN r4484

--- runtime/platform/linux.c
+++ runtime/platform/linux.c
@@ -73,10 +73,12 @@ void *GC_mremap (void *start, size_t oldLength, size_t newLength) {
 /*                       Posix                       */
 /* ------------------------------------------------- */
 
+__attribute__ ((noreturn))
 void Posix_IO_setbin (__attribute__ ((unused)) C_Fd_t fd) {
         die("Posix_IO_setbin not implemented");
 }
 
+__attribute__ ((noreturn))
 void Posix_IO_settext (__attribute__ ((unused)) C_Fd_t fd) {
         die("Posix_IO_settext not implemented");
 }
@@ -85,6 +87,7 @@ void Posix_IO_settext (__attribute__ ((unused)) C_Fd_t fd) {
 /*                      Process                      */
 /* ------------------------------------------------- */
 
+__attribute__ ((noreturn))
 C_Errno_t(C_PId_t) MLton_Process_cwait (__attribute__ ((unused)) C_PId_t pid, 
                                         __attribute__ ((unused)) Ref(C_Status_t) status) {
         die("MLton_Process_cwait not implemented");
