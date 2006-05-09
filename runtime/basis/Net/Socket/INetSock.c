9d5d52fe19e3d1e30d32cd5125e3a432af21da46
Stephen Weeks <sweeks@sweeks.com>
2006-05-09 20:44:22 +0000

    Renamed file statics to avoid spurious warnings when building a cross runtime.
    SVN r4502

--- runtime/basis/Net/Socket/INetSock.c
+++ runtime/basis/Net/Socket/INetSock.c
@@ -11,21 +11,21 @@ Socket_INetSock_toAddr (Vector(Word8_t) in_addr, C_Int_t port,
   *((socklen_t*)addrlen) = sizeof(struct sockaddr_in);
 }
 
-static int port;
-static struct in_addr in_addr;
+static int fromAddr_port;
+static struct in_addr fromAddr_in_addr;
 
 void Socket_INetSock_fromAddr (Vector(Word8_t) addr) {
   struct sockaddr_in *sa = (struct sockaddr_in*)addr;
   
   assert(sa->sin_family == AF_INET);
-  port = sa->sin_port;
-  in_addr = sa->sin_addr;
+  fromAddr_port = sa->sin_port;
+  fromAddr_in_addr = sa->sin_addr;
 }
 
 C_Int_t Socket_INetSock_getPort (void) {
-  return port;
+  return fromAddr_port;
 }
 
 void Socket_INetSock_getInAddr (Array(Word8_t) addr) {
-  *(struct in_addr*)addr = in_addr;
+  *(struct in_addr*)addr = fromAddr_in_addr;
 }
