d3120b5356943a8be54f56ed4dfe564e39f82048
Wesley Terpstra <wesley@terpstra.ca>
2006-05-10 15:17:45 +0000

    fixed for new GC api
    SVN r4519

--- runtime/basis/MLton/Process/spawnp.c
+++ runtime/basis/MLton/Process/spawnp.c
@@ -3,21 +3,21 @@
 #if HAS_SPAWN
 
 C_Errno_t(C_Int_t) MLton_Process_spawnp (NullString8_t p, NullString8Array_t a) {
-  const char      *file;
+  const char      *path;
   const char      *asaved;
-  const char      **args;
+  char            **args;
   int             an;
   int             res;
 
   path = (const char *) p;
   args = (const char **) a;
-  an = GC_arrayNumElements((pointer)a) - 1;
+  an = GC_getArrayLength((pointer)a) - 1;
   asaved = args[an];
   args[an] = (const char *) NULL;
-  result = spawnvp (SPAWN_MODE, path, 
+  res = spawnvp (SPAWN_MODE, path, 
                     (const char * const *)args);
   args[an] = asaved;
-  return res;
+  return (C_Errno_t(C_Int_t))res;
 }
 
 #else
