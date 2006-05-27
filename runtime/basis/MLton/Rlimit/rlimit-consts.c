bd030a64363e22326920c024e50b5fa20897197e
Matthew Fluet <matthew.fluet@gmail.com>
2006-05-27 12:39:06 +0000

    Work around missing RLIMIT_AS.
    SVN r4611

--- runtime/basis/MLton/Rlimit/rlimit-consts.c
+++ runtime/basis/MLton/Rlimit/rlimit-consts.c
@@ -1,6 +1,12 @@
 #include "platform.h"
 
+#if (defined (RLIMIT_AS))
 const C_Int_t MLton_Rlimit_AS = RLIMIT_AS;
+#elif (defined (RLIMIT_DATA))
+const C_Int_t MLton_Rlimit_AS = RLIMIT_DATA;
+#else
+#error MLton_Rlimit_AS not defined
+#endif
 const C_Int_t MLton_Rlimit_CORE = RLIMIT_CORE;
 const C_Int_t MLton_Rlimit_CPU = RLIMIT_CPU;
 const C_Int_t MLton_Rlimit_DATA = RLIMIT_DATA;
