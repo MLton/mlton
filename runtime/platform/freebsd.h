992719ec9a5a237cc06a7460a6a15035b4f9a2e0
Matthew Fluet <matthew.fluet@gmail.com>
2006-06-01 19:42:53 +0000

    Cleaning up includes
    SVN r4626

--- runtime/platform/freebsd.h
+++ runtime/platform/freebsd.h
@@ -6,13 +6,10 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <netinet/udp.h>
 #include <poll.h>
 #include <pwd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/poll.h>
-#include <sys/ptrace.h>
 #include <sys/resource.h>
 #include <sys/socket.h>
 #include <sys/sysctl.h>
