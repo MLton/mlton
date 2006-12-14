91c717a4c857458035fd78869278336b7d0f1c7c
Matthew Fluet <matthew.fluet@gmail.com>
2006-12-14 20:39:54 +0000

    Rename variable in function prototype to match function definition
    SVN r4976

--- runtime/gc/object-size.h
+++ runtime/gc/object-size.h
@@ -9,7 +9,7 @@
 #if (defined (MLTON_GC_INTERNAL_FUNCS))
 
 static inline size_t sizeofArrayNoHeader (GC_state s, GC_arrayLength numElements, 
-                                          uint16_t numNonObjptrs, uint16_t numObjptrs);
+                                          uint16_t bytesNonObjptrs, uint16_t numObjptrs);
 static inline size_t sizeofStackNoHeader (GC_state s, GC_stack stack);
 
 static inline size_t sizeofObject (GC_state s, pointer p);
