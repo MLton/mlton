2a1eb7df6d430c8e88f3719719f31430a1eb65db
Stephen Weeks <sweeks@sweeks.com>
2006-12-28 20:27:17 +0000

    Better error message when fread_safe fails.
    SVN r5001

--- runtime/util/safe.h
+++ runtime/util/safe.h
@@ -47,9 +47,14 @@ static inline void fread_safe (void *buf, size_t size, size_t count, FILE *f) {
   size_t res;
 
   res = fread (buf, size, count, f);
-  if (res != count)
-    diee ("fread (_, %zu, %zu, _) failed (only read %zu).\n",
-          size, count, res);
+  if (res != count) {
+    if (feof (f))
+       fprintf (stderr, "eof\n");
+    else
+       fprintf (stderr, "errno = %d\n", ferror (f));  
+    diee ("fread ("FMTPTR", %zu, %zu, _) failed (only read %zu).\n",
+          (uintptr_t)buf, size, count, res);
+  }
 }
 
 static inline void fwrite_safe (const void *buf, size_t size, size_t count, FILE *f) {
