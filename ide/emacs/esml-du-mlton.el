d298b4bd394f2e58877afa9e8b30a80f6dd8949d
Vesa Karvonen <vesa.a.j.k@gmail>
2007-02-02 14:52:56 +0000

    Don't require sml-mode.
    SVN r5110

--- ide/emacs/esml-du-mlton.el
+++ ide/emacs/esml-du-mlton.el
@@ -4,7 +4,6 @@
 ;; See the file MLton-LICENSE for details.
 
 (require 'def-use-mode)
-(require 'sml-mode)
 (require 'bg-job)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
