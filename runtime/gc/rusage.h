d8d074b9e5f5f735312489289b69271a1d97360e
Matthew Fluet <matthew.fluet@gmail.com>
2007-04-11 20:43:11 +0000

    Regularize handling of gc/*.h file
    SVN r5512

--- runtime/gc/rusage.h
+++ runtime/gc/rusage.h
@@ -6,6 +6,8 @@
  * See the file MLton-LICENSE for details.
  */
 
+#if (defined (MLTON_GC_INTERNAL_FUNCS))
+
 static inline void rusageZero (struct rusage *ru);
 static inline void rusagePlusMax (struct rusage *ru1,
                                   struct rusage *ru2,
@@ -17,3 +19,5 @@ static inline uintmax_t rusageTime (struct rusage *ru);
 static inline uintmax_t getCurrentTime (void);
 static inline void startTiming (struct rusage *ru_start);
 static uintmax_t stopTiming (struct rusage *ru_start, struct rusage *ru_gc);
+
+#endif /* (defined (MLTON_GC_INTERNAL_FUNCS)) */
