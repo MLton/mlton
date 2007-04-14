33493017005a65118fb7ad7ef3022756bb97b079
Matthew Fluet <matthew.fluet@gmail.com>
2007-04-14 18:53:21 +0000

    Fixed aliasing problems with basis/Real/*.c files
    SVN r5525

--- runtime/basis/Real/signBit.c
+++ runtime/basis/Real/signBit.c
@@ -12,32 +12,74 @@ C_Int_t Real64_signBit (Real64_t d) {
 
 #else
 
-#if (defined __i386__)
-
 enum {
-  R32_byte = 3,
-  R64_byte = 7,
+  LITTLE_R32_byte = 3,
+  LITTLE_R64_byte = 7,
 };
 
-#elif (defined __ppc__ || defined __sparc__)
-
 enum {
-  R32_byte = 0,
-  R64_byte = 0,
+  BIG_R32_byte = 0,
+  BIG_R64_byte = 0,
 };
-
-#else
-
-#error Real_signBit not implemented
-
-#endif
 
 C_Int_t Real32_signBit (Real32_t f) {
-  return (((unsigned char *)&f)[R32_byte] & 0x80) >> 7;
+  int R32_byte;
+  if (isBigEndian()) {
+    R32_byte = BIG_R32_byte;
+  } else {
+    R32_byte = LITTLE_R32_byte;
+  }
+
+  /* Using memcpy. 
+   * Technically correct.
+   */
+  unsigned char chars[4];
+  memcpy(chars, &f, sizeof(Real32_t));
+  return (chars[R32_byte] & 0x80) >> 7;
+  /* Using cast; 
+   * Technically correct, as (unsigned char*) may alias.   
+   */
+  /*
+  return (((unsigned char*)(&f))[R32_byte] & 0x80) >> 7;
+  */
+  /* Using union; 
+   * Technically undefined, but widely supported. 
+   */
+  /*
+  union {float f; unsigned char c[4];} fc;
+  fc.f = f;
+  return (fc.c[R32_byte] & 0x80) >> 7;
+  */
 }
 
 C_Int_t Real64_signBit (Real64_t d) {
-  return (((unsigned char *)&d)[R64_byte] & 0x80) >> 7;
+  int R64_byte;
+  if (isBigEndian()) {
+    R64_byte = BIG_R64_byte;
+  } else {
+    R64_byte = LITTLE_R64_byte;
+  }
+
+  /* Using memcpy. 
+   * Technically correct.
+   */
+  unsigned char chars[8];
+  memcpy(chars, &d, sizeof(Real64_t));
+  return (chars[R64_byte] & 0x80) >> 7;
+  /* Using cast; 
+   * Technically correct, as (unsigned char*) may alias.   
+   */
+  /*
+  return (((unsigned char*)(&d))[R64_byte] & 0x80) >> 7;
+  */
+  /* Using union; 
+   * Technically undefined, but widely supported. 
+   */
+  /*
+  union {double d; unsigned char c[8];} dc;
+  dc.d = d;
+  return (dc.c[R64_byte] & 0x80) >> 7;
+  */
 }
 
 #endif
