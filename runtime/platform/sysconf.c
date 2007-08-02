abf64484ab7a6cb197055a0398ab23ceb8b0e9c6
Matthew Fluet <matthew.fluet@gmail.com>
2007-08-02 22:09:29 +0000

    32-bit systems with more than 4GB physical memory might overflow the GC_totalRam computation (yielding actual total RAM `mod` 4GB).  So, compute at uintmax_t and return min of actual total RAM and SIZE_MAX; we can't return more than the virtual address space allows.
    SVN r5815

--- runtime/platform/sysconf.c
+++ runtime/platform/sysconf.c
@@ -1,14 +1,21 @@
 size_t GC_pageSize (void) {
-  long int tmp;
+  long int pageSize;
 
-  tmp = sysconf (_SC_PAGESIZE);
-  return (size_t)tmp;
+  pageSize = sysconf (_SC_PAGESIZE);
+  return (size_t)pageSize;
 }
 
 size_t GC_totalRam (void) {
   size_t pageSize = GC_pageSize ();
-  long int tmp;
+  long int physPages;
+  uintmax_t totalRam;
 
-  tmp = sysconf (_SC_PHYS_PAGES);
-  return pageSize * (size_t)tmp;
+  physPages = sysconf (_SC_PHYS_PAGES);
+  totalRam = (uintmax_t)pageSize * (uintmax_t)physPages;
+  if (sizeof(size_t) >= sizeof(uintmax_t)) {
+    return (uintmax_t)totalRam;
+  } else {
+    totalRam = min(totalRam, (uintmax_t)SIZE_MAX);
+    return (size_t)totalRam;
+  }
 }
