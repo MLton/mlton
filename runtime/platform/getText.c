c22c24c7320c16c2ecc85942f445ce2006b6743d
Wesley Terpstra <wesley@terpstra.ca>
2008-07-31 00:31:08 +0000

    It seems that _init isn't always internal. The others might not be either.
    SVN r6679

--- runtime/platform/getText.c
+++ runtime/platform/getText.c
@@ -1,10 +1,10 @@
 /* To get the beginning and end of the text segment. */
 #if defined(PIC)
-INTERNAL extern unsigned char _init;
+extern unsigned char _init;
 #else
-INTERNAL extern unsigned char _start;
+extern unsigned char _start;
 #endif
-INTERNAL extern unsigned char etext;
+extern unsigned char etext;
 
 code_pointer GC_getTextStart (void) {
 #if defined(PIC)
