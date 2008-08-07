7c45051b2b97a17866488cc43fc4487b249c634e
Wesley Terpstra <wesley@terpstra.ca>
2008-08-07 18:37:16 +0000

    The batch file shouldn't link in -lgdtoa since main.fun does this too.
    SVN r6691

--- package/mingw/mlton.bat
+++ package/mingw/mlton.bat
@@ -29,8 +29,7 @@ set mlton=%lib%\mlton-compile.exe
 set ccopts=-O1 -fno-strict-aliasing -fomit-frame-pointer -w
 set ccopts=%ccopts% -fno-strength-reduce -fschedule-insns -fschedule-insns2
 set ccopts=%ccopts% -malign-functions=5 -malign-jumps=2 -malign-loops=2
-set linkopts=-lgdtoa -lm
-set linkopts=%linkopts% -lgmp -lws2_32 -lkernel32 -lpsapi -lnetapi32
+set linkopts=--lm -lgmp -lws2_32 -lkernel32 -lpsapi -lnetapi32
 
 %mlton% @MLton load-world %world% ram-slop 0.5 -- %lib% -cc %cc% -cc-opt-quote "-I%lib%\include" -cc-opt "%ccopts%" -mlb-path-map "%lib%\mlb-path-map" -link-opt "%linkopts%" %*
 goto :eof
