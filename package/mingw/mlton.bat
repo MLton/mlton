98d8f66b63411e0467b499dfa58c422c0a50687a
Wesley Terpstra <wesley@terpstra.ca>
2008-08-08 14:16:33 +0000

    Don't use double \\s in paths. If gcc is not pathed, the wrapper script makes certain that it is.
    SVN r6696

--- package/mingw/mlton.bat
+++ package/mingw/mlton.bat
@@ -11,17 +11,20 @@ if not exist %dir% (
   goto :end
 ) 
 
-set lib=%dir%\lib\MLton
+set lib=%dir%..\lib\MLton
 if not exist %lib% (
   echo MLton library directory %lib% does not exist
   goto :end
 )
 
-set cc=%dir%\bin\gcc.exe
+set cc=%dir%gcc.exe
 if not exist %cc% (
   echo GCC compiler %cc% does not exist
   goto :end
 )
+
+rem gcc needs to be pathed to find as, ld, etc
+set PATH=%dir%;%PATH%
 
 set world=%lib%\world.mlton
 set mlton=%lib%\mlton-compile.exe
@@ -29,13 +32,13 @@ set mlton=%lib%\mlton-compile.exe
 set ccopts=-O1 -fno-strict-aliasing -fomit-frame-pointer -w
 set ccopts=%ccopts% -fno-strength-reduce -fschedule-insns -fschedule-insns2
 set ccopts=%ccopts% -malign-functions=5 -malign-jumps=2 -malign-loops=2
-set linkopts=--lm -lgmp -lws2_32 -lkernel32 -lpsapi -lnetapi32
+set linkopts=-lm -lgmp -lws2_32 -lkernel32 -lpsapi -lnetapi32
 
 %mlton% @MLton load-world %world% ram-slop 0.5 -- %lib% -cc %cc% -cc-opt-quote "-I%lib%\include" -cc-opt "%ccopts%" -mlb-path-map "%lib%\mlb-path-map" -link-opt "%linkopts%" %*
 goto :eof
 
 :setdir
-set dir=%1%..\
+set dir=%1%
 GOTO :eof
 
 :downlevel
