d9f842c922b2656df22d3a9786a5d8f46dcb2230
Matthew Fluet <matthew.fluet@gmail.com>
2008-08-20 00:43:53 +0000

    Unused function
    SVN r6777

--- runtime/gc/virtual-memory.c
+++ runtime/gc/virtual-memory.c
@@ -29,10 +29,3 @@ static inline void GC_memcpy (pointer src, pointer dst, size_t size) {
     return;
   memcpy (dst, src, size);
 }
-
-static inline void GC_memmove (pointer src, pointer dst, size_t size) {
-  if (DEBUG_DETAILED)
-    fprintf (stderr, "GC_memmove ("FMTPTR", "FMTPTR", %"PRIuMAX")\n",
-             (uintptr_t)src, (uintptr_t)dst, (uintmax_t)size);
-  memmove (dst, src, size);
-}
