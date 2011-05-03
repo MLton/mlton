86a30e3ff85c64a54cda3907ad82d487c10296f9
Matthew Fluet <matthew.fluet@gmail.com>
2011-05-03 20:30:21 +0000

    Be less paranoid in emergency output functions.
    In particular, when stderr is not accessible (e.g., killed pipe), an
    uncaught exception that is printed by the top-level handler induces an
    infinite loop.
    
    SVN r7529

--- runtime/basis/MLton/bug.c
+++ runtime/basis/MLton/bug.c
@@ -4,9 +4,7 @@
 void MLton_bug (String8_t msg) {
   uintmax_t size = GC_getArrayLength ((pointer)msg);
   fprintf (stderr, "MLton bug: ");
-  unless (0 == size)
-    while (1 != fwrite ((const void*)msg, (size_t)size, 1, stderr))
-      /* nothing */;
+  fwrite ((const void*)msg, (size_t)size, 1, stderr);
   fprintf (stderr, "\nPlease send a bug report to MLton@mlton.org.\n");
   exit (2);
 }
--- runtime/basis/Stdio.c
+++ runtime/basis/Stdio.c
@@ -2,18 +2,12 @@
 
 void Stdio_printStderr (String8_t s) {
   uintmax_t size = GC_getArrayLength ((pointer)s);
-  if (0 == size)
-    return;
-  while (1 != fwrite ((const void*)s, (size_t)size, 1, stderr))
-    /* nothing */;
+  fwrite ((const void*)s, (size_t)size, 1, stderr);
 }
 
 void Stdio_printStdout (String8_t s) {
   uintmax_t size = GC_getArrayLength ((pointer)s);
-  if (0 == size)
-    return;
-  while (1 != fwrite ((const void*)s, (size_t)size, 1, stdout))
-    /* nothing */;
+  fwrite ((const void*)s, (size_t)size, 1, stdout);
 }
 
 void Stdio_print (String8_t s) {
