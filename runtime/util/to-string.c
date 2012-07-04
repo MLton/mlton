6c70b4bb3b8b543589e0251b425d0c8cf5c8285d
Matthew Fluet <matthew.fluet@gmail.com>
2012-07-04 11:03:05 +0000

    Simplify {intmax,uintmax}ToCommaString.
    Also avoids manifestly unreachable code with
    'switch (sizeof(intmax_t)) {...}' and
    'switch (sizeof(uintmax_t)) {...}'.
    
    SVN r7589

--- runtime/util/to-string.c
+++ runtime/util/to-string.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 2004-2008 Henry Cejtin, Matthew Fluet, Suresh
+/* Copyright (C) 2012 Matthew Fluet.
+ * Copyright (C) 2004-2008 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  *
  * MLton is released under a BSD-style license.
@@ -11,7 +12,7 @@ const char* boolToString (bool b) {
   return b ? "TRUE" : "FALSE";
 }
 
-#define BUF_SIZE 81
+#define BUF_SIZE 64
 char* intmaxToCommaString (intmax_t n) {
   static char buf1[BUF_SIZE];
   static char buf2[BUF_SIZE];
@@ -21,58 +22,36 @@ char* intmaxToCommaString (intmax_t n) {
   static char *bufs[] = {buf1, buf2, buf3, buf4, buf5};
   static int bufIndex = 0;
   static char *buf;
-  int i;
+  char tmp[BUF_SIZE];
+  int i, j, k, l;
 
   buf = bufs[bufIndex++];
   bufIndex %= 5;
 
-  i = BUF_SIZE - 1;
-  buf[i--] = '\000';
-
-  if (0 == n)
-    buf[i--] = '0';
-  else if (INTMAX_MIN == n) {
-    const char* s;
-    /* must treat INTMAX_MIN specially, because I negate stuff later */
-    switch (sizeof(intmax_t)) {
-    case 1:
-      s = "-128";
-      break;
-    case 2:
-      s = "-32,768";
-      break;
-    case 4:
-      s = "-2,147,483,648";
-      break;
-    case 8:
-      s = "-9,223,372,036,854,775,808";
-      break;
-    case 16:
-      s = "-170,141,183,460,469,231,731,687,303,715,884,105,728";
-      break;
-    default:
-      die ("intmaxToCommaString: sizeof(intmax_t) = %"PRIuMAX"",
-           (uintmax_t)sizeof(intmax_t));
-      break;
-    }
-    strncpy (buf + 1, s, strlen(s) + 1);
-    i = 0;
+  l = snprintf(tmp, BUF_SIZE, "%"PRIdMAX, n);
+  if (tmp[0] == '-') {
+    buf[0] = '-';
+    i = 1;
+    j = 1;
+    k = (l - 1) % 3;
   } else {
-    intmax_t m;
-
-    if (n > 0)
-      m = n;
-    else
-      m = -n;
-
-    while (m > 0) {
-      buf[i--] = (char)((m % 10) + '0');
-      m = m / 10;
-      if (i % 4 == 0 and m > 0) buf[i--] = ',';
-    }
-    if (n < 0) buf[i--] = '-';
+    i = 0;
+    j = 0;
+    k = l % 3;
   }
-  return buf + i + 1;
+  buf[j++] = tmp[i++];
+  k--;
+  while (tmp[i] != '\000') {
+    if (k == 0) {
+      buf[j++] = ',';
+      k = 3;
+    }
+    buf[j++] = tmp[i++];
+    k--;
+  }
+  buf[j] = '\000';
+
+  return buf;
 }
 
 char* uintmaxToCommaString (uintmax_t n) {
@@ -84,22 +63,28 @@ char* uintmaxToCommaString (uintmax_t n) {
   static char *bufs[] = {buf1, buf2, buf3, buf4, buf5};
   static int bufIndex = 0;
   static char *buf;
-  int i;
+  char tmp[BUF_SIZE];
+  int i, j, k, l;
 
   buf = bufs[bufIndex++];
   bufIndex %= 5;
 
-  i = BUF_SIZE - 1;
-  buf[i--] = '\000';
-  if (0 == n)
-    buf[i--] = '0';
-  else {
-    while (n > 0) {
-      buf[i--] = (char)((n % 10) + '0');
-      n = n / 10;
-      if (i % 4 == 0 and n > 0) buf[i--] = ',';
+  l = snprintf(tmp, BUF_SIZE, "%"PRIuMAX, n);
+  i = 0;
+  j = 0;
+  k = l % 3;
+  buf[j++] = tmp[i++];
+  k--;
+  while (tmp[i] != '\000') {
+    if (k == 0) {
+      buf[j++] = ',';
+      k = 3;
     }
+    buf[j++] = tmp[i++];
+    k--;
   }
-  return buf + i + 1;
+  buf[j] = '\000';
+
+  return buf;
 }
 #undef BUF_SIZE
