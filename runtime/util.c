2fdba82e4f07f5708bd1c7390ab66fcb9d54ee3a
Matthew Fluet <matthew.fluet@gmail.com>
2012-07-04 11:02:40 +0000

    Eliminate unused #define.
    SVN r7585

--- runtime/util.c
+++ runtime/util.c
@@ -1,10 +1,10 @@
-/* Copyright (C) 1999-2008 Henry Cejtin, Matthew Fluet, Suresh
+/* Copyright (C) 2012 Matthew Fluet.
+ * Copyright (C) 1999-2008 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  *
  * MLton is released under a BSD-style license.
  * See the file MLton-LICENSE for details.
  */
 
-#define MLTON_UTIL
 #include "util/die.c"
 #include "util/to-string.c"
