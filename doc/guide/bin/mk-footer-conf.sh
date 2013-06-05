7d95aa32f5d7a16662bc0afa480ed4e4aececd81
Matthew Fluet <matthew.fluet@gmail.com>
2013-06-05 09:11:50 -0400

    Update scripts to use unique prefixes with csplit.

--- doc/guide/bin/mk-footer-conf.sh
+++ doc/guide/bin/mk-footer-conf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-csplit -s $1 '%\[footer\]%' '/<.html>/+1'
-(cat xx00 | sed 's/XHTML/HTML5/') > $2
-rm xx00 xx01
+prefix="$2"
+csplit -f ${prefix} -n 2 -s $1 '%\[footer\]%' '/ifdef::icons\[\]/' '%endif::icons\[\]%+1' '/<\/html>/+1'
+(cat ${prefix}00 | sed 's/XHTML/HTML5/'; cat ${prefix}01) > $2
+rm ${prefix}00 ${prefix}01 ${prefix}02
--- doc/guide/bin/mk-header-conf.sh
+++ doc/guide/bin/mk-header-conf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-csplit -s $1 '%\[header\]%' '/<body.*>/+1' '/<div id="content">/+1'
-(cat xx00 ; echo "<div id=\"banner\">"; echo "template::[banner-body]"; echo "</div>"; cat xx01) > $2
-rm xx00 xx01 xx02
+prefix="$2"
+csplit -f ${prefix} -n 2 -s $1 '%\[header\]%' '/<body.*>/+1' '/<div id="content">/+1'
+(cat ${prefix}00 ; echo "<div id=\"banner\">"; echo "template::[banner-body]"; echo "</div>"; cat ${prefix}01) > $2
+rm ${prefix}00 ${prefix}01 ${prefix}02
