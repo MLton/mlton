f3a946a6dca118183bda7c7668e463bd168a562c
Matthew Fluet <matthew.fluet@gmail.com>
2013-06-07 18:35:47 -0400

    Shorten anchors in References and Index pages.

--- doc/guide/bin/mk-index.sh
+++ doc/guide/bin/mk-index.sh
@@ -8,12 +8,12 @@ echo ":no-log-link:"
 echo ":no-edit-link:"
 echo ""
 for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
-    echo "<!Link(${ul}_Index,${ul})>"
+    echo "<!Link(${ul}${ul}${ul},${ul})>"
 done
 for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
     echo ""
     ll=$(echo ${ul} | tr "[:upper:]" "[:lower:]")
-    echo "== <!Anchor(${ul}_Index)>${ul} =="
+    echo "== <!Anchor(${ul}${ul}${ul})>${ul} =="
     echo ""
     for page in ${pages}; do
         case ${page} in
