385a834a8b25aa627aa762ec3a816ce026a51bf7
Matthew Fluet <matthew.fluet@gmail.com>
2013-06-07 18:26:21 -0400

    Generate a better wiki/Index page.

--- doc/guide/bin/mk-index.sh
+++ doc/guide/bin/mk-index.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+pages=$(echo $@ | sort -f)
+
 echo "Index"
 echo "====="
+echo ":no-log-link:"
+echo ":no-edit-link:"
 echo ""
-for f in $(cd txt; ls *.txt | sort -f); do
-    n="$(basename $f .txt)"
-    echo "* <:$n:>"
+for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
+    echo "<!Link(${ul}_Index,${ul})>"
+done
+for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
+    echo ""
+    ll=$(echo ${ul} | tr "[:upper:]" "[:lower:]")
+    echo "== <!Anchor(${ul}_Index)>${ul} =="
+    echo ""
+    for page in ${pages}; do
+        case ${page} in
+            ${ul}*) ;;
+            ${ll}*) ;;
+            *) continue;;
+        esac
+        echo "* <:${page}:>"
+    done
 done
