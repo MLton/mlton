08c0a8233338a17556bf4409d9006a1efe9fa64e
Matthew Fluet <matthew.fluet@gmail.com>
2013-06-12 13:08:00 -0400

    Updates to replacements.
    Rename Link replacement as XRef replacement.

--- doc/guide/bin/mk-index.sh
+++ doc/guide/bin/mk-index.sh
@@ -8,7 +8,7 @@ echo ":no-log-link:"
 echo ":no-edit-link:"
 echo ""
 for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
-    echo "<!Link(${ul}${ul}${ul},${ul})>"
+    echo "<!XRef(${ul}${ul}${ul},${ul})>"
 done
 for ul in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
     echo ""
