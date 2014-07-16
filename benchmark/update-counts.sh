2f0d3881c68bf2948828a7c1794e689efaa689eb
Matthew Fluet <matthew.fluet@gmail.com>
2014-07-15 22:15:07 -0400

    Use GNU time in update-counts.sh script.

--- benchmark/update-counts.sh
+++ benchmark/update-counts.sh
@@ -1,3 +1,23 @@
+#!/usr/bin/env bash
+
+# set -e
+
+die () {
+        echo "$1" >&2
+        exit 1
+}
+
+if $(which gtime) --version >/dev/null 2>&1; then
+        time=$(which gtime)
+elif $(which gnutime) --version >/dev/null 2>&1; then
+        time=$(which gnutime)
+elif $(which time) --version 2>&1 | grep -q GNU; then
+        time=$(which time)
+else
+        die 'Can'\''t find GNU time'
+fi
+
+
 minTime="30.0"
 
 bench="barnes-hut boyer checksum count-graphs DLXSimulator even-odd fft fib flat-array hamlet imp-for knuth-bendix lexgen life logic mandelbrot matrix-multiply md5 merge mlyacc model-elimination mpuz nucleic output1 peek psdes-random ratio-regions ray raytrace simple smith-normal-form tailfib tak tensor tsp tyan vector-concat vector-rev vliw wc-input1 wc-scanStream zebra zern"
@@ -18,7 +38,7 @@ for prog in $bench; do
     n=1;
     m=1;
     k=1;
-    t=$(/usr/bin/time -o $prog.time --format "%U + %S" ./$prog $n 1>/dev/null 2>/dev/null; cat $prog.time | grep -v "Command exited" | bc)
+    t=$($time -o $prog.time --format "%U + %S" ./$prog $n 1>/dev/null 2>/dev/null; cat $prog.time | grep -v "Command exited" | bc)
     while [ "$(echo "$t < $minTime" | bc)" = "1" ]; do
         if [ $n -lt $m ]; then
             n=$(($n+$k))
@@ -31,7 +51,7 @@ for prog in $bench; do
                 n=$(($n+$k))
             fi
         fi
-        t=$(/usr/bin/time -o $prog.time --format "%U + %S" ./$prog $n 1>/dev/null 2>/dev/null; cat $prog.time | grep -v "Command exited" | bc)
+        t=$($time -o $prog.time --format "%U + %S" ./$prog $n 1>/dev/null 2>/dev/null; cat $prog.time | grep -v "Command exited" | bc)
     done
 
     rm $prog $prog.main.sml $prog.time
