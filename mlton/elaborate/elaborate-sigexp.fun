MLton/mlton

commit 263865ebe3678bb5e5eac3506e5e51b30aae6174

Add Interface.Flexible.{admitsEquality,new,setAdmitsEquality}
Clarify that only flexible tycons can have equality status changed.

mlton/elaborate/elaborate-sigexp.fun
@@ -52,6 +52,7 @@ in
    structure AdmitsEquality = AdmitsEquality
    structure Cons = Cons
    structure Kind = Kind
+   structure FlexibleTycon = FlexibleTycon
    structure Scheme = Scheme
    structure Status = Status
    structure Tycon = Tycon
@@ -205,10 +206,10 @@ fun elaborateTypedescs (typedescs: {tycon: Ast.Tycon.t,
              then AdmitsEquality.Sometimes
              else AdmitsEquality.Never
        val kind = Kind.Arity (Vector.length tyvars)
-       val tycon =
-          Tycon.make {admitsEquality = admitsEquality,
-                      hasCons = false,
-                      kind = kind}
+       val flex = FlexibleTycon.new {admitsEquality = admitsEquality,
+                                     hasCons = false,
+                                     kind = kind}
+       val tycon = Tycon.Flexible flex
     in
        Env.extendTycon (E, name, TypeStr.tycon (tycon, equality))
     end)
@@ -249,13 +250,14 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
           let
              val arity = Vector.length tyvars
              val kind = Kind.Arity arity
-             val tycon = Tycon.make {admitsEquality = AdmitsEquality.Sometimes,
-                                     hasCons = true,
-                                     kind = kind}
-             val _ =
-                Env.extendTycon (E, name, TypeStr.tycon (tycon, false))
+             val flex = FlexibleTycon.new {admitsEquality = AdmitsEquality.Sometimes,
+                                           hasCons = false,
+                                           kind = kind}
+             val tycon = Tycon.Flexible flex
+             val _ = Env.extendTycon (E, name, TypeStr.tycon (tycon, false))
           in
              {cons = cons,
+              flex = flex,
               name = name,
               tycon = tycon,
               tyvars = tyvars}
@@ -271,7 +273,7 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
       val _ = elabTypBind (withtypes, E, {sequential = false})
       val datatypes =
          Vector.map
-         (datatypes, fn {cons, name, tycon, tyvars} =>
+         (datatypes, fn {cons, flex, name, tycon, tyvars} =>
           let
              val cons =
                 Vector.map
@@ -283,6 +285,7 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
                    tyvars = tyvars}))
           in
              {cons = cons,
+              flex = flex,
               name = name,
               tycon = tycon}
           end)
@@ -292,7 +295,7 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
           let
              val _ =
                 Vector.foreach
-                (datatypes, fn {cons, tycon, ...} =>
+                (datatypes, fn {cons, flex, ...} =>
                  let
                     val isEquality = ref true
                     val () =
@@ -310,13 +313,13 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
                          end))
                     datatype z = datatype AdmitsEquality.t
                  in
-                    case Tycon.admitsEquality tycon of
+                    case FlexibleTycon.admitsEquality flex of
                        Always => Error.bug "ElaborateSigexp.elaborateDatBind: Always"
                      | Never => ()
                      | Sometimes =>
                           if !isEquality
                              then ()
-                             else (Tycon.setAdmitsEquality (tycon, Never)
+                             else (FlexibleTycon.setAdmitsEquality (flex, Never)
                                    ; change := true)
                  end)
           in
@@ -327,7 +330,7 @@ fun elaborateDatBind (datBind: DatBind.t, E): unit =
       val () = loop ()
       val () =
          Vector.foreach
-         (datatypes, fn {cons, name, tycon} =>
+         (datatypes, fn {cons, name, tycon, ...} =>
           let
              val cons =
                 Vector.map

mlton/elaborate/interface.fun
@@ -124,12 +124,14 @@ structure FlexibleTycon =
       local
          fun make f = f o fields
       in
-         val admitsEquality = make #admitsEquality
+         val admitsEquality = ! o make #admitsEquality
          val defnRef = make #defn
          val defn = ! o defnRef
          val hasCons = make #hasCons
          val kind = make #kind
          val plist = make #plist
+         fun setAdmitsEquality (f, ae) =
+            (make #admitsEquality f) := ae
          val specsRef = make #specs
          val specs = ! o specsRef
       end
@@ -171,12 +173,6 @@ structure FlexibleTycon =
                            plist = PropertyList.new (),
                            specs = ref specs})
 
-      fun new {admitsEquality: AdmitsEquality.t, defn: Defn.t,
-               hasCons: bool, kind: Kind.t}: t =
-         make {admitsEquality = admitsEquality,
-               defn = defn, hasCons = hasCons, kind = kind,
-               specs = AppendList.empty}
-
       fun pushSpec (fc, region) =
          let
             val specsRef = specsRef fc
@@ -195,13 +191,8 @@ structure Tycon =
 
       fun admitsEquality c =
          case c of
-            Flexible f => ! (FlexibleTycon.admitsEquality f)
+            Flexible f => FlexibleTycon.admitsEquality f
           | Rigid c => Etycon.admitsEquality c
-
-      fun setAdmitsEquality (c, ae) =
-         case c of
-            Flexible f => FlexibleTycon.admitsEquality f := ae
-          | Rigid _ => Error.bug "Interface.Tycon.setAdmitsEquality: Rigid"
 
       val arrow = fromEnv Etycon.arrow
 
@@ -695,6 +686,14 @@ structure FlexibleTycon =
    struct
       open FlexibleTycon
 
+      fun new {admitsEquality: AdmitsEquality.t,
+               hasCons: bool, kind: Kind.t}: t =
+         make {admitsEquality = admitsEquality,
+               defn = Defn.undefined,
+               hasCons = hasCons,
+               kind = kind,
+               specs = AppendList.empty}
+
       fun realize (fc, typeStr) =
          let
             val defn = defnRef fc
@@ -746,17 +745,6 @@ structure FlexibleTycon =
             Defn.Realized s => ETypeStr s
           | Defn.TypeStr s => TypeStr s
           | _ => Error.bug "Interface.FlexibleTycon.realization"
-   end
-
-structure Tycon =
-   struct
-      open Tycon
-
-      fun make {admitsEquality, hasCons, kind} =
-         Flexible (FlexibleTycon.new {admitsEquality = admitsEquality,
-                                      defn = Defn.undefined,
-                                      hasCons = hasCons,
-                                      kind = kind})
    end
 
 structure Scheme =

mlton/elaborate/interface.sig
@@ -58,26 +58,28 @@ signature INTERFACE =
             type typeStr
             type t
 
+            val admitsEquality: t -> AdmitsEquality.t
             val dest: t -> {admitsEquality: AdmitsEquality.t,
                             hasCons: bool,
                             kind: Kind.t}
             val layout: t -> Layout.t
             val plist: t -> PropertyList.t
+            val new: {admitsEquality: AdmitsEquality.t,
+                      hasCons: bool,
+                      kind: Kind.t} -> t
             val realize: t * EnvTypeStr.t -> unit
             datatype realization =
                ETypeStr of EnvTypeStr.t
              | TypeStr of typeStr
             val realization: t -> realization
+            val setAdmitsEquality: t * AdmitsEquality.t -> unit
          end
       structure Tycon:
          sig
             datatype t =
                Flexible of FlexibleTycon.t
              | Rigid of EnvTycon.t
-
             val admitsEquality: t -> AdmitsEquality.t
-            val make: {admitsEquality: AdmitsEquality.t, hasCons: bool, kind: Kind.t} -> t
-            val setAdmitsEquality: t * AdmitsEquality.t -> unit
          end
       structure Record: RECORD
       sharing Record = Ast.SortedRecord

commit 18af09f039c5bf406ccea2d01d0295ee39902627

Fix bug introduced by 263865e

mlton/elaborate/elaborate-sigexp.fun
@@ -256,7 +256,7 @@ fun elaborateDatBind (datBind: DatBind.t, E, nest): unit =
              val prettyDefault =
                 concat (List.separate (rev (Ast.Tycon.toString name :: nest), "."))
              val flex = FlexibleTycon.new {admitsEquality = AdmitsEquality.Sometimes,
-                                           hasCons = false,
+                                           hasCons = true,
                                            kind = kind,
                                            prettyDefault = prettyDefault}
              val tycon = Tycon.Flexible flex