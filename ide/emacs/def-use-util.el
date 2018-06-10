ac14b84533b6434b1e0b39c4e9923bb5f643b232
Matthew Fluet <matthew.fluet@gmail.com>
2018-06-10 16:55:57 -0400

    Replace `labels` with `cl-labels`
    The macro `labels` is obsolete (as of 24.3).

--- ide/emacs/def-use-util.el
+++ ide/emacs/def-use-util.el
@@ -147,7 +147,7 @@ one of the modification times is nil."
   "Returns non-nil iff the file attributes of `attr1' are different than
 the file attributes of `attr2'.  Note that this also returns nil when either
 one of the file attributes is nil."
-  (labels ((nequal (i) (not (equal (nth i attr1) (nth i attr2)))))
+  (cl-labels ((nequal (i) (not (equal (nth i attr1) (nth i attr2)))))
     (and attr1 attr2
          (or (def-use-attr-newer? attr1 attr2)
              (nequal 7)  ;; size
