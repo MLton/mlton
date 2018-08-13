7cd21e989ffd567ca4f8d906f7d5fcbdb5813f26
Daman Morris <damanm72@gmail.com>
2018-08-13 14:32:21 -0400

    Refactor C overflow primitives and FFI definitions

--- runtime/basis/Word/Word-check.h
+++ runtime/basis/Word/Word-check.h
@@ -1,30 +1,30 @@
-#define add_overflow_b(x, y, size, onOverflow, onSuccess)       \
+#define add_overflow_b(x, y, kind, onOverflow, onSuccess)       \
   do {                                                          \
-    WordS##size res;                                            \
+    Word##kind res;                                             \
     if (__builtin_add_overflow(x, y, &res)) {                   \
       onOverflow;                                               \
     }                                                           \
     onSuccess;                                                  \
   } while (0)
-#define sub_overflow_b(x, y, size, onOverflow, onSuccess)       \
+#define sub_overflow_b(x, y, kind, onOverflow, onSuccess)       \
   do {                                                          \
-    WordS##size res;                                            \
+    Word##kind res;                                             \
     if (__builtin_sub_overflow(x, y, &res)) {                   \
       onOverflow;                                               \
     }                                                           \
     onSuccess;                                                  \
   } while (0)
-#define mul_overflow_b(x, y, size, onOverflow, onSuccess)       \
+#define mul_overflow_b(x, y, kind, onOverflow, onSuccess)       \
   do {                                                          \
-    WordS##size res;                                            \
+    Word##kind res;                                             \
     if (__builtin_mul_overflow(x, y, &res)) {                   \
       onOverflow;                                               \
     }                                                           \
     onSuccess;                                                  \
   } while (0)
-#define neg_overflow_b(x, size, onOverflow, onSuccess)          \
+#define neg_overflow_b(x, kind, onOverflow, onSuccess)          \
   do {                                                          \
-    Word##size res;                                             \
+    Word##kind res;                                             \
     if (__builtin_sub_overflow(0, x, &res)) {                   \
       onOverflow;                                               \
     }                                                           \
@@ -36,26 +36,26 @@
  */
 
 #define WordS_addCheckBody(size, x, y, doOverflow, doSuccess)   \
-  add_overflow_b(x, y, size, doOverflow, doSuccess)
+  add_overflow_b(x, y, S##size, doOverflow, doSuccess)
 #define WordS_addCheckBodyCX(size, c, x, doOverflow, doSuccess) \
   WordS_addCheckBody(size, c, x, doOverflow, doSuccess)
 
 #define WordU_addCheckBody(size, x, y, doOverflow, doSuccess)   \
-  add_overflow_b(x, y, size, doOverflow, doSuccess)
+  add_overflow_b(x, y, U##size, doOverflow, doSuccess)
 #define WordU_addCheckBodyCX(size, c, x, doOverflow, doSuccess) \
   WordU_addCheckBody(size, c, x, doOverflow, doSuccess)
 
 #define WordS_mulCheckBody(size, x, y, doOverflow, doSuccess)   \
-  mul_overflow_b(x, y, size, doOverflow, doSuccess)
+  mul_overflow_b(x, y, S##size, doOverflow, doSuccess)
 
 #define WordU_mulCheckBody(size, x, y, doOverflow, doSuccess)   \
-  mul_overflow_b(x, y, size, doOverflow, doSuccess)
+  mul_overflow_b(x, y, U##size, doOverflow, doSuccess)
 
 #define WordS_negCheckBody(size, x, doOverflow, doSuccess)      \
-  neg_overflow_b(x, size, doOverflow, doSuccess)
+  neg_overflow_b(x, S##size, doOverflow, doSuccess)
 
 #define WordS_subCheckBody(size, x, y, doOverflow, doSuccess)   \
-  sub_overflow_b(x, y, size, doOverflow, doSuccess)
+  sub_overflow_b(x, y, S##size, doOverflow, doSuccess)
 #define WordS_subCheckBodyCX(size, c, x, doOverflow, doSuccess) \
   WordS_subCheckBody(size, c, x, doOverflow, doSuccess)
 
@@ -84,36 +84,36 @@
 #define WordS_addCheckP(size)                                           \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool WordS##size##_addCheckP (WordS##size x, WordS##size y) {         \
-    add_overflow_b(x, y, size, return TRUE, return FALSE);              \
+    add_overflow_b(x, y, S##size, return TRUE, return FALSE);           \
   }
 
 #define WordU_addCheckP(size)                                           \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool WordU##size##_addCheckP (WordU##size x, WordU##size y) {         \
-    add_overflow_b(x, y, size, return TRUE, return FALSE);              \
+    add_overflow_b(x, y, U##size, return TRUE, return FALSE);           \
   }
 
 #define WordS_mulCheckP(size)                                           \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool WordS##size##_mulCheckP (WordS##size x, WordS##size y) {         \
-    mul_overflow_b(x, y, size, return TRUE, return FALSE);              \
+    mul_overflow_b(x, y, S##size, return TRUE, return FALSE);           \
   }
 #define WordU_mulCheckP(size)                                           \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool WordU##size##_mulCheckP (WordU##size x, WordU##size y) {         \
-    mul_overflow_b(x, y, size, return TRUE, return FALSE);              \
+    mul_overflow_b(x, y, U##size, return TRUE, return FALSE);           \
   }
 
 #define Word_negCheckP(size)                                            \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool Word##size##_negCheckP (WordS##size x) {                         \
-    neg_overflow_b(x, size, return TRUE, return FALSE);                 \
+    neg_overflow_b(x, S##size, return TRUE, return FALSE);              \
   }
 
 #define WordS_subCheckP(size)                                           \
   MLTON_CODEGEN_STATIC_INLINE                                           \
   Bool WordS##size##_subCheckP (WordS##size x, WordS##size y) {         \
-    sub_overflow_b(x, y, size, return TRUE, return FALSE);              \
+    sub_overflow_b(x, y, S##size, return TRUE, return FALSE);           \
   }
 
 #define all(size)                               \
