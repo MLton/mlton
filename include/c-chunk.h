acdede14a4f6223a99922dc9c9ab314ade2a21be
Matthew Fluet <matthew.fluet@gmail.com>
2019-04-12 22:05:06 -0400

    Remove debug diagnostics for checking tail call optimization

--- include/c-chunk.h
+++ include/c-chunk.h
@@ -47,8 +47,8 @@
 #define Chunk(n)                                \
         DeclareChunk(n) {                       \
                 if (DEBUG_CCODEGEN)             \
-                        fprintf (stderr, "%s:%d: Chunk%d(nextBlock = %d)  %p %p\n", \
-                                        __FILE__, __LINE__, n, (int)nextBlock, __builtin_return_address(0), __builtin_frame_address(0)); \
+                        fprintf (stderr, "%s:%d: Chunk%d(nextBlock = %d)\n", \
+                                        __FILE__, __LINE__, n, (int)nextBlock); \
                 Pointer frontier;               \
                 Pointer stackTop;
 
