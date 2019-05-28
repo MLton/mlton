MLton/mlton

commit 136f3573c7a9b93dce80a80dd435406cd0b96af0

Omit allocating registers for unused args of Cont, CReturn, and Handler blocks
It is not necessary to allocate registers for unused args of Cont,
CReturn, and Handler blocks.

This fixes unused variable warnings with the C codegen.

mlton/backend/backend.fun
@@ -79,9 +79,9 @@ structure VarOperand =
              | Const oper => seq [str "Const ", M.Operand.layout oper]
          end
 
-      val operand: t -> M.Operand.t =
-         fn Allocate {operand, ...} => valOf (!operand)
-          | Const oper => oper
+      val operand: t -> M.Operand.t option =
+         fn Allocate {operand, ...} => !operand
+          | Const oper => SOME oper
    end
 
 structure ByteSet = UniqueSet (val cacheSize: int = 1
@@ -439,8 +439,9 @@ let
                       fn {operand, ...} =>
                       Layout.record [("operand", VarOperand.layout operand)])
          varInfo
-      val varOperand: Var.t -> M.Operand.t =
+      val varOperandOpt: Var.t -> M.Operand.t option =
          VarOperand.operand o #operand o varInfo
+      val varOperand: Var.t -> M.Operand.t = valOf o varOperandOpt
       (* Hash tables for uniquifying globals. *)
       local
          fun 'a make {equals: 'a * 'a -> bool,
@@ -1016,16 +1017,15 @@ let
                               val srcs = callReturnStackOffsets (args, #2, size)
                               val (dsts', srcs') =
                                  Vector.unzip
-                                 (Vector.keepAllMapi
-                                  (args, fn (i, (arg, _)) =>
-                                   let
-                                      val dst = varOperand arg
-                                   in
-                                      if Vector.exists (live, fn var =>
-                                                        M.Operand.equals (var, dst))
-                                         then SOME (dst, M.Operand.StackOffset (Vector.sub (srcs, i)))
-                                         else NONE
-                                   end))
+                                 (Vector.keepAllMap2
+                                  (args, srcs, fn ((arg, _), src) =>
+                                   case varOperandOpt arg of
+                                      NONE => NONE
+                                    | SOME dst =>
+                                         if Vector.exists (live, fn var =>
+                                                           M.Operand.equals (var, dst))
+                                            then SOME (dst, M.Operand.StackOffset src)
+                                            else NONE))
                            in
                               (M.Kind.Cont {args = Vector.map (srcs, Live.StackOffset),
                                             frameInfo = valOf (frameInfo label)},
@@ -1037,9 +1037,9 @@ let
                               val dst =
                                  case Vector.length args of
                                     0 => NONE
-                                  | 1 => SOME (operandLive
-                                               (varOperand
-                                                (#1 (Vector.sub (args, 0)))))
+                                  | 1 => Option.map
+                                         (varOperandOpt (#1 (Vector.first args)),
+                                          operandLive)
                                   | _ => Error.bug "Backend.genBlock: CReturn"
                            in
                               (M.Kind.CReturn {dst = dst,
@@ -1050,16 +1050,18 @@ let
                            end
                       | R.Kind.Handler =>
                            let
-                              val dsts = Vector.map (args, varOperand o #1)
-                              val handles = raiseOperands (Vector.map (dsts, M.Operand.ty))
+                              val handles = raiseOperands (Vector.map (args, #2))
                               val (dsts', srcs') =
                                  Vector.unzip
-                                 (Vector.keepAllMapi
-                                  (dsts, fn (i, dst) =>
-                                   if Vector.exists (live, fn var =>
-                                                     M.Operand.equals (var, dst))
-                                      then SOME (dst, Live.toOperand (Vector.sub (handles, i)))
-                                      else NONE))
+                                 (Vector.keepAllMap2
+                                  (args, handles, fn ((arg, _), h) =>
+                                   case varOperandOpt arg of
+                                      NONE => NONE
+                                    | SOME dst =>
+                                         if Vector.exists (live, fn var =>
+                                                           M.Operand.equals (var, dst))
+                                            then SOME (dst, Live.toOperand h)
+                                            else NONE))
                            in
                               (M.Kind.Handler
                                {frameInfo = valOf (frameInfo label),

mlton/backend/allocate-registers.fun
@@ -514,7 +514,11 @@ fun allocate {formalsStackOffsets,
                                         "bad size ",
                                         Bytes.toString size,
                                         " in ", Label.toString label])
-             val _ = Vector.foreach (args, fn (x, _) => allocateVar (x, a))
+             val _ = Vector.foreach (args, fn (x, _) =>
+                                     if Vector.exists (begin, fn y =>
+                                                       Var.equals (x, y))
+                                        then allocateVar (x, a)
+                                        else ())
              (* Must compute live after allocateVar'ing the args, since that
               * sets the operands for the args.
               *)

commit 428321adb92b17c1a955eeb43191c3cdce1833dc

Omit copying unused args of `Jump` blocks at a `Goto` transfer
Fixes a bug that was introduced by 136f3573c.  The intended purpose of 136f3573c
was to omit copying values returned via the ML stack on non-tail calls to
`Cont` (normal return) and `Handle` (exceptional return) blocks and to omit
copying a result returned from a `CCall` to a `CReturn` block; essentially, the
receiving block is required to accept those arguments due to calling convention,
but the argument may not be otherwise used, so the copies are extraneous.

The implementation of 136f3573c simply omitted allocating a location (Machine IR
stack slot or register (local)) for any unused argument of any block.  The
assumption was that only `Cont`, `Handle`, and `CReturn` blocks could have
unused arguments; it was assumed that previous optimizations would eliminate any
argument of a `Jump` block that was unused.  However, with RedPRL, there is (at
least one) `Jump` block with an unused argument; that argument is not assigned a
location, so the `Option` exception is raised when trying to implement a `Goto`
transfer that copies the actual operands to the formal arguments (and the
corresponding formal argument doesn't exist).

The fix is straight forward: only copy the actual arguments that correspond to
formal arguments that are used; this is essentially the same filtering that is
used to implement the prologue of `Cont` and `Handle` blocks.

Closes MLton/mlton#305.

mlton/backend/backend.fun
@@ -727,8 +727,6 @@ let
             val returns =
                Option.map (returns, fn ts =>
                            callReturnStackOffsets (ts, fn t => t, Bytes.zero))
-            fun labelArgOperands (l: R.Label.t): M.Operand.t vector =
-               Vector.map (#args (labelInfo l), varOperand o #1)
             fun newVarInfo (x, ty: Type.t) =
                let
                   val operand =
@@ -946,9 +944,18 @@ let
                            (setupArgs, transfer)
                         end
                    | R.Transfer.Goto {dst, args} =>
-                        (parallelMove {srcs = translateOperands args,
-                                       dsts = labelArgOperands dst},
-                         M.Transfer.Goto dst)
+                        let
+                           val (dsts', srcs') =
+                              Vector.unzip
+                              (Vector.keepAllMap2
+                               (#args (labelInfo dst), args, fn ((dst, _), src) =>
+                                case varOperandOpt dst of
+                                   NONE => NONE
+                                 | SOME dst => SOME (dst, translateOperand src)))
+                        in
+                           (parallelMove {srcs = srcs', dsts = dsts'},
+                            M.Transfer.Goto dst)
+                        end
                    | R.Transfer.Raise srcs =>
                         (M.Statement.moves {dsts = Vector.map (valOf raises,
                                                                Live.toOperand),