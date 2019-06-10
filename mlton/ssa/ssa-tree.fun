MLton/mlton

commit 9c5082f6275b04d42c99d35daf829e34cabee36c

Deprecate SSA(2) Arith transfers

mlton/ssa/ssa-tree.fun
@@ -764,12 +764,12 @@ structure Return =
 structure Transfer =
    struct
       datatype t =
-         Arith of {prim: Type.t Prim.t,
+         (* DEPRECATED Arith of {prim: Type.t Prim.t,
                    args: Var.t vector,
                    overflow: Label.t, (* Must be nullary. *)
                    success: Label.t, (* Must be unary. *)
-                   ty: Type.t}
-       | Bug (* MLton thought control couldn't reach here. *)
+                   ty: Type.t} *)
+         Bug (* MLton thought control couldn't reach here. *)
        | Call of {args: Var.t vector,
                   func: Func.t,
                   return: Return.t}
@@ -786,8 +786,7 @@ structure Transfer =
 
       (* Vals to determine the size for inline.fun and loop optimization*)
       val size =
-         fn Arith {args, ...} => 1 + Vector.length args
-          | Bug => 1
+         fn Bug => 1
           | Call {args, ...} => 1 + Vector.length args
           | Case {cases, ...} => 1 + Cases.length cases
           | Goto {args, ...} => 1 + Vector.length args
@@ -800,11 +799,7 @@ structure Transfer =
             fun vars xs = Vector.foreach (xs, var)
          in
             case t of
-               Arith {args, overflow, success, ...} =>
-                  (vars args
-                   ; label overflow 
-                   ; label success)
-             | Bug => ()
+               Bug => ()
              | Call {func = f, args, return, ...} =>
                   (func f
                    ; Return.foreachLabel (return, label)
@@ -835,13 +830,7 @@ structure Transfer =
             fun fxs xs = Vector.map (xs, fx)
          in
             case t of
-               Arith {prim, args, overflow, success, ty} =>
-                  Arith {prim = prim,
-                         args = fxs args,
-                         overflow = fl overflow,
-                         success = fl success,
-                         ty = ty}
-             | Bug => Bug
+               Bug => Bug
              | Call {func, args, return} =>
                   Call {func = func, 
                         args = fxs args,
@@ -893,11 +882,7 @@ structure Transfer =
                seq [Prim.layoutFull (prim, Type.layout), str " ", layoutArgs args]
          in
             case t of
-               Arith {prim, args, overflow, success, ty}=>
-                  seq [str "arith ", Type.layout ty, str " ", Label.layout success, str " ",
-                       paren (layoutPrim {prim = prim, args = args}),
-                       str " handle Overflow => ", Label.layout overflow]
-             | Bug => str "bug"
+               Bug => str "bug"
              | Call {func, args, return} =>
                   let
                      val call = seq [Func.layout func, str " ", layoutArgs args]
@@ -949,19 +934,7 @@ structure Transfer =
                pure (fn return => pure {func = func, args = args, return = return})))
          in
             any
-            [Arith <$>
-             (kw "arith" *>
-              Type.parse >>= (fn ty =>
-              Label.parse >>= (fn success =>
-              paren (Prim.parseFull Type.parse >>= (fn prim =>
-                     parseArgs >>= (fn args =>
-                     pure (prim, args)))) >>= (fn (prim, args) =>
-              kw "handle" *> kw "Overflow" *> sym "=>" *>
-              Label.parse >>= (fn overflow =>
-              pure {prim = prim, args = args,
-                    overflow = overflow,
-                    success = success, ty = ty}))))),
-             Bug <$ kw "bug",
+            [Bug <$ kw "bug",
              Call <$>
              (kw "call" *>
               any [kw "dead" *> parseCall >>= (fn mkCall => mkCall Return.Dead),
@@ -997,15 +970,7 @@ structure Transfer =
 
       fun equals (e: t, e': t): bool =
          case (e, e') of
-            (Arith {prim, args, overflow, success, ...},
-             Arith {prim = prim', args = args', 
-                    overflow = overflow', success = success', ...}) =>
-               Prim.equals (prim, prim') andalso
-               varsEquals (args, args') andalso
-               Label.equals (overflow, overflow') andalso
-               Label.equals (success, success')
-          | (Bug, Bug) => true
-          | (Call {func, args, return}, 
+            (Call {func, args, return}, 
              Call {func = func', args = args', return = return'}) =>
                Func.equals (func, func') andalso
                varsEquals (args, args') andalso
@@ -1037,10 +1002,7 @@ structure Transfer =
          fun hash2 (w1: Word.t, w2: Word.t) = Hash.combine (w1, w2)
       in
          val hash: t -> Word.t =
-            fn Arith {args, overflow, success, ...} =>
-                  hashVars (args, hash2 (Label.hash overflow,
-                                         Label.hash success))
-             | Bug => bug
+            fn Bug => bug
              | Call {func, args, return} =>
                   hashVars (args, hash2 (Func.hash func, Return.hash return))
              | Case {test, cases, default} =>
@@ -1418,10 +1380,7 @@ structure Function =
                          edge (from, labelNode to, label, style)
                       val () =
                          case transfer of
-                            Arith {overflow, success, ...} =>
-                               (edge (success, "", Solid)
-                                ; edge (overflow, "Overflow", Dashed))
-                          | Bug => ()
+                            Bug => ()
                           | Call {return, ...} =>
                                let
                                   val _ =
@@ -2162,8 +2121,7 @@ structure Program =
                  | _ => ()
              fun loopTransfer t =
                 case t of
-                   Arith {prim, ...} => f prim
-                 | Runtime {prim, ...} => f prim
+                   Runtime {prim, ...} => f prim
                  | _ => ()
              val _ = Vector.foreach (globals, loopStatement)
              val _ =

mlton/backend/ssa-to-rssa.fun
@@ -848,113 +848,7 @@ fun convert (program as S.Program.T {functions, globals, main, ...},
       fun translateTransfer (t: S.Transfer.t): (Statement.t list *
                                                 Transfer.t) =
          case t of
-            S.Transfer.Arith {args, overflow, prim, success, ty} =>
-               let
-                  val prim = translatePrim prim
-                  val ty = valOf (toRtype ty)
-                  val res = Var.newNoname ()
-                  val noOverflow =
-                     newBlock
-                     {args = Vector.new0 (),
-                      kind = Kind.Jump,
-                      statements = Vector.new0 (),
-                      transfer = (Transfer.Goto
-                                  {dst = success,
-                                   args = (Vector.new1
-                                           (Var {var = res, ty = ty}))})}
-               in
-                  if codegenImplementsPrim prim
-                     then ([],
-                           Transfer.Arith {dst = res,
-                                           args = vos args,
-                                           overflow = overflow,
-                                           prim = prim,
-                                           success = noOverflow,
-                                           ty = ty})
-                     else
-                        let
-                           datatype z = datatype Prim.Name.t
-                           fun doOperCheckCF (operCheck) =
-                              let
-                                 val operCheckCF =
-                                    case Name.cFunction operCheck of
-                                       NONE =>
-                                          Error.bug
-                                          (concat ["SsaToRssa.translateTransfer: ",
-                                                   "unimplemented arith:",
-                                                   Name.toString operCheck])
-                                     | SOME operCheckCF => operCheckCF
-                                 val afterOperCheck =
-                                    let
-                                       val checkRes = Var.newNoname ()
-                                    in
-                                       newBlock
-                                       {args = Vector.new1 (checkRes, Type.bool),
-                                        kind = Kind.CReturn {func = operCheckCF},
-                                        statements = Vector.new0 (),
-                                        transfer = (Transfer.ifBool
-                                                    (Var {var = checkRes,
-                                                          ty = Type.bool},
-                                                     {falsee = noOverflow,
-                                                      truee = overflow}))}
-                                    end
-                              in
-                                 Transfer.CCall
-                                 {args = vos args,
-                                  func = operCheckCF,
-                                  return = SOME afterOperCheck}
-                              end
-                           fun doOperCF (oper, operCheck) =
-                              let
-                                 val operCF =
-                                    case Name.cFunction oper of
-                                       NONE =>
-                                          Error.bug
-                                          (concat ["SsaToRssa.translateTransfer: ",
-                                                   "unimplemented arith:",
-                                                   Name.toString oper])
-                                     | SOME operCF => operCF
-                                 val afterOper =
-                                    newBlock
-                                    {args = Vector.new1 (res, ty),
-                                     kind = Kind.CReturn {func = operCF},
-                                     statements = Vector.new0 (),
-                                     transfer = doOperCheckCF operCheck}
-                              in
-                                 Transfer.CCall
-                                 {args = vos args,
-                                  func = operCF,
-                                  return = SOME afterOper}
-                              end
-                           fun doPrim prim =
-                              [Statement.PrimApp
-                               {dst = SOME (res, ty),
-                                prim = prim,
-                                args = vos args}]
-                           fun doit (prim, operCheck) =
-                              if codegenImplementsPrim prim
-                                 then (doPrim prim, doOperCheckCF operCheck)
-                              else ([], doOperCF (Prim.name prim, operCheck))
-                        in
-                           case Prim.name prim of
-                              (* DEPRECATED Word_addCheck (s, sg) =>
-                                 doit (Prim.wordAdd s,
-                                       Word_addCheck (s, sg))
-                            | Word_mulCheck (s, sg) =>
-                                 doit (Prim.wordMul (s, sg),
-                                       Word_mulCheck (s, sg))
-                            | Word_negCheck s =>
-                                 doit (Prim.wordNeg s,
-                                       Word_negCheck s)
-                            | Word_subCheck (s, sg) =>
-                                 doit (Prim.wordSub s,
-                                       Word_subCheck (s, sg)) *)
-                              _ => Error.bug (concat ["SsaToRssa.translateTransfer: ",
-                                                      "strange arith:",
-                                                      Name.toString (Prim.name prim)])
-                        end
-               end
-          | S.Transfer.Bug => ([], Transfer.bug ())
+            S.Transfer.Bug => ([], Transfer.bug ())
           | S.Transfer.Call {func, args, return} =>
                let
                   datatype z = datatype S.Return.t

mlton/closure-convert/closure-convert.fun
@@ -948,133 +948,126 @@ fun closureConvert
                                       targs = targs,
                                       ty = ty}
                   in
-                     if Prim.mayOverflow prim
-                        then simple (Dexp.arith
-                                     {args = Vector.map (args, convertVarExp),
-                                      overflow = Dexp.raisee (convertVar overflow),
-                                      prim = prim,
-                                      ty = ty})
-                     else
-                        let
-                           datatype z = datatype Prim.Name.t
-                        in
-                           simple
-                           (case Prim.name prim of
-                               Array_update =>
-                                  let
-                                     val a = varExpInfo (arg 0)
-                                     val y = varExpInfo (arg 2)
-                                     val v = Value.deArray (VarInfo.value a)
-                                  in
-                                     primApp (v1 (valueType v),
-                                              v3 (convertVarInfo a,
-                                                  convertVarExp (arg 1),
-                                                  coerce (convertVarInfo y,
-                                                          VarInfo.value y, v)))
-                                  end
-                             | MLton_eq =>
-                                  let
-                                     val a0 = varExpInfo (arg 0)
-                                     val a1 = varExpInfo (arg 1)
-                                     fun doit () =
-                                        primApp (v1 (valueType (VarInfo.value a0)),
-                                                 v2 (convertVarInfo a0,
-                                                     convertVarInfo a1))
-                                  in
-                                     case (Value.dest (VarInfo.value a0),
-                                           Value.dest (VarInfo.value a1)) of
-                                        (Value.Lambdas l, Value.Lambdas l') =>
-                                           if Lambdas.equals (l, l')
-                                              then doit () 
-                                           else Dexp.falsee
-                                      | _ => doit ()
-                                  end
-                             | MLton_equal =>
-                                  let
-                                     val a0 = varExpInfo (arg 0)
-                                     val a1 = varExpInfo (arg 1)
-                                     fun doit () =
-                                        primApp (v1 (valueType (VarInfo.value a0)),
-                                                 v2 (convertVarInfo a0,
-                                                     convertVarInfo a1))
-                                  in
-                                     case (Value.dest (VarInfo.value a0),
-                                           Value.dest (VarInfo.value a1)) of
-                                        (Value.Lambdas l, Value.Lambdas l') =>
-                                           if Lambdas.equals (l, l')
-                                              then doit () 
-                                           else Dexp.falsee
-                                      | _ => doit ()
-                                  end
-                             | MLton_handlesSignals =>
-                                  if handlesSignals
-                                     then Dexp.truee
-                                  else Dexp.falsee
-                             | Ref_assign =>
-                                  let
-                                     val r = varExpInfo (arg 0)
-                                     val y = varExpInfo (arg 1)
-                                     val v = Value.deRef (VarInfo.value r)
-                                  in
-                                     primApp (v1 (valueType v),
-                                              v2 (convertVarInfo r,
-                                                  coerce (convertVarInfo y,
-                                                          VarInfo.value y, v)))
-                                  end
-                             | Ref_ref =>
-                                  let
-                                     val y = varExpInfo (arg 0)
-                                     val v = Value.deRef v
-                                  in
-                                     primApp (v1 (valueType v),
-                                              v1 (coerce (convertVarInfo y,
-                                                          VarInfo.value y, v)))
-                                  end
-                             | MLton_serialize =>
-                                  let
-                                     val y = varExpInfo (arg 0)
-                                     val v =
-                                        Value.serialValue (Vector.first targs)
-                                  in
-                                     primApp (v1 (valueType v),
-                                              v1 (coerce (convertVarInfo y,
-                                                          VarInfo.value y, v)))
-                                  end
-                             | Vector_vector =>
-                                  let
-                                     val ys = Vector.map (args, varExpInfo)
-                                     val v = Value.deVector v
-                                  in
-                                     primApp (v1 (valueType v),
-                                              Vector.map (ys, fn y =>
-                                                          coerce (convertVarInfo y,
-                                                                  VarInfo.value y, v)))
-                                  end
-                             | Weak_new =>
-                                  let
-                                     val y = varExpInfo (arg 0)
-                                     val v = Value.deWeak v
-                                  in
-                                     primApp (v1 (valueType v),
-                                              v1 (coerce (convertVarInfo y,
-                                                          VarInfo.value y, v)))
-                                  end
-                             | _ =>
-                                  let
-                                     val args = Vector.map (args, varExpInfo)
-                                  in
-                                     primApp
-                                     (Prim.extractTargs
-                                      (prim,
-                                       {args = Vector.map (args, varInfoType),
-                                        result = ty,
-                                        typeOps = {deArray = Type.deArray,
-                                                   deArrow = fn _ => Error.bug "ClosureConvert.convertPrimExp: deArrow",
-                                                   deRef = Type.deRef,
-                                                   deVector = Type.deVector,
-                                                   deWeak = Type.deWeak}}),
-                                       Vector.map (args, convertVarInfo))
-                                  end)
+                      let
+                         datatype z = datatype Prim.Name.t
+                      in
+                         simple
+                         (case Prim.name prim of
+                             Array_update =>
+                                let
+                                   val a = varExpInfo (arg 0)
+                                   val y = varExpInfo (arg 2)
+                                   val v = Value.deArray (VarInfo.value a)
+                                in
+                                   primApp (v1 (valueType v),
+                                            v3 (convertVarInfo a,
+                                                convertVarExp (arg 1),
+                                                coerce (convertVarInfo y,
+                                                        VarInfo.value y, v)))
+                                end
+                           | MLton_eq =>
+                                let
+                                   val a0 = varExpInfo (arg 0)
+                                   val a1 = varExpInfo (arg 1)
+                                   fun doit () =
+                                      primApp (v1 (valueType (VarInfo.value a0)),
+                                               v2 (convertVarInfo a0,
+                                                   convertVarInfo a1))
+                                in
+                                   case (Value.dest (VarInfo.value a0),
+                                         Value.dest (VarInfo.value a1)) of
+                                      (Value.Lambdas l, Value.Lambdas l') =>
+                                         if Lambdas.equals (l, l')
+                                            then doit () 
+                                         else Dexp.falsee
+                                    | _ => doit ()
+                                end
+                           | MLton_equal =>
+                                let
+                                   val a0 = varExpInfo (arg 0)
+                                   val a1 = varExpInfo (arg 1)
+                                   fun doit () =
+                                      primApp (v1 (valueType (VarInfo.value a0)),
+                                               v2 (convertVarInfo a0,
+                                                   convertVarInfo a1))
+                                in
+                                   case (Value.dest (VarInfo.value a0),
+                                         Value.dest (VarInfo.value a1)) of
+                                      (Value.Lambdas l, Value.Lambdas l') =>
+                                         if Lambdas.equals (l, l')
+                                            then doit () 
+                                         else Dexp.falsee
+                                    | _ => doit ()
+                                end
+                           | MLton_handlesSignals =>
+                                if handlesSignals
+                                   then Dexp.truee
+                                else Dexp.falsee
+                           | Ref_assign =>
+                                let
+                                   val r = varExpInfo (arg 0)
+                                   val y = varExpInfo (arg 1)
+                                   val v = Value.deRef (VarInfo.value r)
+                                in
+                                   primApp (v1 (valueType v),
+                                            v2 (convertVarInfo r,
+                                                coerce (convertVarInfo y,
+                                                        VarInfo.value y, v)))
+                                end
+                           | Ref_ref =>
+                                let
+                                   val y = varExpInfo (arg 0)
+                                   val v = Value.deRef v
+                                in
+                                   primApp (v1 (valueType v),
+                                            v1 (coerce (convertVarInfo y,
+                                                        VarInfo.value y, v)))
+                                end
+                           | MLton_serialize =>
+                                let
+                                   val y = varExpInfo (arg 0)
+                                   val v =
+                                      Value.serialValue (Vector.first targs)
+                                in
+                                   primApp (v1 (valueType v),
+                                            v1 (coerce (convertVarInfo y,
+                                                        VarInfo.value y, v)))
+                                end
+                           | Vector_vector =>
+                                let
+                                   val ys = Vector.map (args, varExpInfo)
+                                   val v = Value.deVector v
+                                in
+                                   primApp (v1 (valueType v),
+                                            Vector.map (ys, fn y =>
+                                                        coerce (convertVarInfo y,
+                                                                VarInfo.value y, v)))
+                                end
+                           | Weak_new =>
+                                let
+                                   val y = varExpInfo (arg 0)
+                                   val v = Value.deWeak v
+                                in
+                                   primApp (v1 (valueType v),
+                                            v1 (coerce (convertVarInfo y,
+                                                        VarInfo.value y, v)))
+                                end
+                           | _ =>
+                                let
+                                   val args = Vector.map (args, varExpInfo)
+                                in
+                                   primApp
+                                   (Prim.extractTargs
+                                    (prim,
+                                     {args = Vector.map (args, varInfoType),
+                                      result = ty,
+                                      typeOps = {deArray = Type.deArray,
+                                                 deArrow = fn _ => Error.bug "ClosureConvert.convertPrimExp: deArrow",
+                                                 deRef = Type.deRef,
+                                                 deVector = Type.deVector,
+                                                 deWeak = Type.deWeak}}),
+                                     Vector.map (args, convertVarInfo))
+                                end)
                         end
                   end
              | SprimExp.Profile e => simple (Dexp.profile e)

mlton/ssa/analyze.fun
@@ -61,15 +61,7 @@ fun 'a analyze
                         shouldReturns: 'a vector option,
                         shouldRaises: 'a vector option): unit =
         (case t of
-            Arith {prim, args, overflow, success, ty} =>
-               (coerces ("arith overflow", Vector.new0 (), labelValues overflow)
-                ; coerce {from = primApp {prim = prim,
-                                          targs = Vector.new0 (),
-                                          args = values args,
-                                          resultType = ty,
-                                          resultVar = NONE},
-                          to = Vector.sub (labelValues success, 0)})
-          | Bug => ()
+            Bug => ()
           | Call {func, args, return, ...} =>
                let
                   val {args = formals, raises, returns} = funcInfo func

mlton/ssa/analyze2.fun
@@ -61,14 +61,7 @@ fun 'a analyze
                         shouldReturns: 'a vector option,
                         shouldRaises: 'a vector option): unit =
         (case t of
-            Arith {prim, args, overflow, success, ty} =>
-               (coerces ("arith overflow", Vector.new0 (), labelValues overflow)
-                ; coerce {from = primApp {prim = prim,
-                                          args = values args,
-                                          resultType = ty,
-                                          resultVar = NONE},
-                          to = Vector.sub (labelValues success, 0)})
-          | Bug => ()
+            Bug => ()
           | Call {func, args, return, ...} =>
                let
                   val {args = formals, raises, returns} = funcInfo func

commit 6ac37e58d62d25a482e937ebb5d1be610019dacd

Fixup `Ssa.Transfer.equals` (broken in 9c5082f6)

mlton/ssa/ssa-tree.fun
@@ -750,7 +750,8 @@ structure Transfer =
 
       fun equals (e: t, e': t): bool =
          case (e, e') of
-            (Call {func, args, return}, 
+            (Bug, Bug) => true
+          | (Call {func, args, return}, 
              Call {func = func', args = args', return = return'}) =>
                Func.equals (func, func') andalso
                varsEquals (args, args') andalso