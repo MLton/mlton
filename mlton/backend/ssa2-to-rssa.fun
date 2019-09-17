MLton/mlton

commit 4a49d4d88ab1775a02958f4744090814056477d2

Refactor static alloc/init controls
New controls are:

 * `-static-alloc-wordvector-consts {true|false}`

   Controls whether or not `WordXVector` constants are converted to
   statics (with `ImmStatic` location) at `Ssa2ToRssa`.

 * `-static-init-arrays {true|false}`

   Controls whether or not `Array_alloc` primitives are converted
   to statics (with `MutStatic` or `Heap` location) at `Ssa2ToRssa`.

 * `-static-init-objects {none|staticAllocOnly|all}`

   Controls whether or not `Object` expressions are converted to
   statics at `Ssa2ToRssa`.  If `staticAllocOnly`, then an object that
   would be converted to a static with `Heap` location is not
   converted to a static.

mlton/backend/ssa2-to-rssa.fun
@@ -1665,32 +1665,18 @@ fun convert (program as S.Program.T {functions, globals, main, ...},
                   | Control.Static =>
                        not (isMutable andalso Option.exists (toRtype elt, Type.isObjptr))
                   | Control.None => false
-            fun validCon con =
-              case (con,
-                    !Control.staticAllocObjects,
-                    !Control.staticAllocVectors) of
-                   (S.ObjectCon.Con _, true, _) => true
-                 | (S.ObjectCon.Tuple, true, _) => true
-                 | (S.ObjectCon.Sequence, _, true) => true
-                 | _ => false
             fun getLocation (ty, isEmpty) =
                case S.Type.dest ty of
-                    (* Important: isMutable is a proxy for hasIdentity,
-                     * so we need to preserve it even if there are no fields *)
-                    S.Type.Object {args, con} =>
-                     if validCon con andalso
-                        (isEmpty orelse
-                           Vector.forall
-                           (S.Prod.dest args, validArg))
-                     then
-                        if S.Prod.someIsMutable args
-                           then Static.Location.MutStatic
-                        else Static.Location.ImmStatic
-                     else
-                        Static.Location.Heap
-                  | _ => Error.bug
-                     "translateGlobalStatics.getLocation: non-object"
-
+                  S.Type.Object {args, ...} =>
+                     (* Important: isMutable is a proxy for hasIdentity,
+                      * so we need to preserve it even if there are no fields
+                      *)
+                     if isEmpty orelse Vector.forall (S.Prod.dest args, validArg)
+                     then if S.Prod.someIsMutable args
+                             then Static.Location.MutStatic
+                             else Static.Location.ImmStatic
+                     else Static.Location.Heap
+                | _ => Error.bug "translateGlobalStatics.getLocation: non-object"
             fun getObjptrTycon ty =
                let
                   fun fail () = Error.bug "translateGlobalStatics.getObjptrTycon"
@@ -1702,35 +1688,38 @@ fun convert (program as S.Program.T {functions, globals, main, ...},
                           | NONE => fail ())
                    | NONE => fail ()
                end
-            fun makeWordXVector (ty, location, wxv) =
+            fun makeWordXVector {data, location, ty} =
                let
                   val tycon = getObjptrTycon ty
                in
-                  (SOME o Static o Static.vector)
-                  {data=wxv, tycon=tycon,
-                   location=location}
+                  (Static o Static.vector)
+                  {data = data,
+                   location = location,
+                   tycon = tycon}
                end
-            fun makeSequence (ty, location, length) =
+            fun makeSequence {length, location, ty} =
                let
                   val tycon = getObjptrTycon ty
-                  val elemSize =
+                  val eltSize =
                      case Vector.sub (objectTypes, ObjptrTycon.index tycon) of
-                          ObjectType.Sequence {elt, ...} => Type.width elt
-                        | _ => Error.bug "translateGlobalStatics.makeSequence: strange sequence tycon"
-                  val bytes = Bytes.fromInt (length * Bits.toInt elemSize)
+                        ObjectType.Sequence {elt, ...} => Bits.toBytes (Type.width elt)
+                      | _ => Error.bug "translateGlobalStatics.makeSequence: strange sequence tycon"
                in
-                  (SOME o Static o Static.sequence)
-                  {length=length, tycon=tycon,
-                   totalSize=bytes, location=location}
+                  (Static o Static.sequence)
+                  {eltSize = eltSize,
+                   length = length,
+                   location = location,
+                   tycon = tycon}
                end
-            fun makeObject (ty, location, {args, con}) =
-               static {args=args, con=con,
-                  elem=fn v =>
-                  case globalStatic v of
-                     SOME static => static
-                   | NONE => Error.bug "translateGlobalStatics.makeObject.elem",
-                  location=location,
-                  objectTy=ty}
+            fun makeObject {args, con, location, ty} =
+               static {args = args,
+                       con = con,
+                       elem = (fn v =>
+                               case globalStatic v of
+                                  SOME static => static
+                                | NONE => Error.bug "translateGlobalStatics.makeObject.elem"),
+                       location = location,
+                       objectTy = ty}
             fun translateBind (st, {exp, ty, var}) =
                case var of
                   NONE => pushKeep st
@@ -1748,59 +1737,73 @@ fun convert (program as S.Program.T {functions, globals, main, ...},
                      fun real r =
                         (setGlobalStatic (var, SOME (Real r));
                          pushKeep st)
-                     fun 'a static (mk: S.Type.t * location * 'a ->
-                                    PackedRepresentation.StaticOrElem.t option, isEmpty, a: 'a) =
-                        let
-                           val location = getLocation (ty, isEmpty)
-                           val so = mk (ty, location, a)
-                           val rty = toRtype ty
-                        in
-                           case (so, rty) of
-                               (SOME s, SOME rty) =>
-                                 (case (s, location) of
-                                      (* address of heap static not a valid elem *)
-                                      (Static s, Heap) =>
-                                       pushStatic (var, ty, rty, s)
-                                    | (Static s, _) =>
-                                       (setGlobalStatic (var, SOME (Address var));
-                                        pushStatic (var, ty, rty, s))
-                                    | (Elem e, _) =>
-                                       (setGlobalStatic (var, SOME e);
-                                        pushKeep st))
-                             | _ => keep ()
-                        end
+                     fun 'a static (soe: PackedRepresentation.StaticOrElem.t) =
+                        case toRtype ty of
+                           SOME rty =>
+                              (case soe of
+                                  Static s =>
+                                     (case Static.location s of
+                                         Heap =>
+                                            (* address of heap static not a valid elem *)
+                                            pushStatic (var, ty, rty, s)
+                                       | _ =>
+                                            (setGlobalStatic (var, SOME (Address var));
+                                             pushStatic (var, ty, rty, s)))
+                                | Elem e =>
+                                     (setGlobalStatic (var, SOME e);
+                                      pushKeep st))
+                         | _ => keep ()
                   in
                      case exp of
                         S.Exp.Const c =>
                            (case c of
-                               Const.WordVector wxv => static (makeWordXVector, false, wxv)
+                               Const.WordVector wxv =>
+                                  if !Control.staticAllocWordVectorConsts
+                                     then static (makeWordXVector {data = wxv,
+                                                                   location = Static.Location.ImmStatic,
+                                                                   ty = ty})
+                                     else keep ()
                              | Const.Word w => word w
                              | Const.Real r => real r
                              | _ => keep ())
                       | S.Exp.Inject {variant, ...} =>
                            copy variant
                       | S.Exp.Object {args, con} =>
-                           let
-                              val location = getLocation (ty, false)
-                           in
-                              if Vector.exists (args, Option.isNone o globalStatic)
-                              (* For objects, there's no reason to statically initalize,
-                               * since they're so small and irregular *)
-                              orelse location = Static.Location.Heap
-                                 then keep ()
-                              else static (makeObject, false, {args=args, con=con})
-                           end
+                           (case !Control.staticInitObjects of
+                               NONE => keep ()
+                             | SOME staticInitHeapObjects =>
+                                  if Vector.exists (args, Option.isNone o globalStatic)
+                                     then keep ()
+                                     else let
+                                             val location = getLocation (ty, false)
+                                          in
+                                             if location <> Static.Location.Heap
+                                                orelse staticInitHeapObjects
+                                                then case makeObject {args = args,
+                                                                      con = con,
+                                                                      location = location,
+                                                                      ty = ty} of
+                                                        NONE => keep ()
+                                                      | SOME soe => static soe
+                                                else ()
+                                           end)
                       | S.Exp.PrimApp {args, prim, ...} =>
                            (case Prim.name prim of
                                Prim.Name.Array_alloc _ =>
-                               let
-                                  val length = (globalStatic o Vector.first) args
-                               in
-                                  case (length, !Control.staticAllocArrays) of
-                                     (SOME (Word l), true) =>
-                                        static (makeSequence, WordX.isZero l, WordX.toInt l)
-                                   | _ => keep ()
-                               end
+                                  let
+                                     val length = (globalStatic o Vector.first) args
+                                  in
+                                     case (length, !Control.staticInitArrays) of
+                                        (SOME (Word l), true) =>
+                                           let
+                                              val location = getLocation (ty, WordX.isZero l)
+                                           in
+                                              static (makeSequence {length = WordX.toInt l,
+                                                                    location = location,
+                                                                    ty = ty})
+                                           end
+                                      | _ => keep ()
+                                  end
                              | _ => keep ())
                       | S.Exp.Var v => copy v
                       | S.Exp.Select _ => keep ()

mlton/backend/static.fun
@@ -116,11 +116,13 @@ functor Static (S: STATIC_STRUCTS): STATIC =
             location = location,
             metadata = sequenceMetadata (tycon, WordXVector.length data)}
 
-      fun sequence {length, location, totalSize, tycon} =
-         T {data = Data.Empty totalSize,
-            location = location,
-            metadata = sequenceMetadata (tycon, length)}
-
-
+      fun sequence {eltSize, length, location, tycon} =
+         let
+            val dataSize = Bytes.fromInt (length * Bytes.toInt eltSize)
+         in
+            T {data = Data.Empty dataSize,
+               location = location,
+               metadata = sequenceMetadata (tycon, length)}
+         end
 
    end

mlton/backend/static.sig
@@ -59,9 +59,9 @@ signature STATIC =
       val object: {elems: ('a Data.Elem.t) list,
                    location: Location.t,
                    tycon: ObjptrTycon.t} -> 'a t
-      val sequence: {length: int,
+      val sequence: {eltSize: Bytes.t,
+                     length: int,
                      location: Location.t,
-                     totalSize: Bytes.t,
                      tycon: ObjptrTycon.t} -> 'a t
       val vector: {data: WordXVector.t,
                    location: Location.t,

mlton/control/control-flags.sig
@@ -400,9 +400,9 @@ signature CONTROL_FLAGS =
        | Static
        | None
       val staticAllocInternalPtrs: staticAllocInternalPtrs ref
-      val staticAllocArrays: bool ref
-      val staticAllocObjects: bool ref
-      val staticAllocVectors: bool ref
+      val staticAllocWordVectorConsts: bool ref
+      val staticInitArrays: bool ref
+      val staticInitObjects: bool option ref
 
       (* List of pass names to stop at. *)
       val stopPasses: Regexp.Compiled.t list ref

mlton/control/control-flags.sml
@@ -1217,18 +1217,18 @@ val staticAllocInternalPtrs =
             default = Static,
             toString = StaticAlloc.Objptrs.toString}
 
-val staticAllocArrays =
-   control {name = "staticAllocArrays",
+val staticAllocWordVectorConsts =
+   control {name = "staticAllocWordVectorConsts",
             default = true,
             toString = Bool.toString}
-val staticAllocObjects =
-   control {name = "staticAllocObjects",
+val staticInitArrays =
+   control {name = "staticInitArrays",
             default = true,
             toString = Bool.toString}
-val staticAllocVectors =
-   control {name = "staticAllocVectors",
-            default = true,
-            toString = Bool.toString}
+val staticInitObjects =
+   control {name = "staticInitObjects",
+            default = SOME false,
+            toString = Option.toString Bool.toString}
 
 structure SplitTypesBool =
    struct

mlton/main/main.fun
@@ -814,25 +814,6 @@ fun makeOptions {usage} =
         SpaceString (fn s => showDefUse := SOME s)),
        (Expert, "show-types", " {true|false}", "show types in ILs",
         boolRef showTypes),
-       (Expert, "static-alloc-internal-ptrs", " {all|static|none}",
-        "which pointers to allow in statically allocated values",
-        SpaceString (fn s =>
-                     staticAllocInternalPtrs :=
-                     (case s of
-                         "all" => Control.All
-                       | "none" => Control.None
-                       | "static" => Control.Static
-                       | _ => usage (concat ["invalid ",
-                       "-static-alloc-internal-ptrs flag: ", s])))),
-       (Expert, "static-alloc-arrays", " {true|false}",
-        "Allow arrays to be statically allocated",
-        boolRef staticAllocArrays),
-       (Expert, "static-alloc-objects", " {true|false}",
-        "Allow objects to be statically allocated",
-        boolRef staticAllocObjects),
-       (Expert, "static-alloc-vectors", " {true|false}",
-        "Allow vectors to be statically allocated",
-        boolRef staticAllocVectors),
        (Expert, "split-types-bool", " {smart|always|never}",
         "bool type splitting method",
         SpaceString (fn s =>
@@ -862,6 +843,31 @@ fun makeOptions {usage} =
                    Result.Yes () => ()
                  | Result.No s' => usage (concat ["invalid -ssa2-passes arg: ", s']))
           | NONE => Error.bug "ssa2 optimization passes missing")),
+       (Expert, "static-alloc-internal-ptrs", " {all|static|none}",
+        "which pointers to allow in statically allocated values",
+        SpaceString (fn s =>
+                     staticAllocInternalPtrs :=
+                     (case s of
+                         "all" => Control.All
+                       | "none" => Control.None
+                       | "static" => Control.Static
+                       | _ => usage (concat ["invalid ",
+                       "-static-alloc-internal-ptrs flag: ", s])))),
+       (Expert, "static-alloc-wordvector-consts", " {true|false}",
+        "Allow word-vector constants (strings) to be statically allocated",
+        boolRef staticAllocWordVectorConsts),
+       (Expert, "static-init-arrays", " {true|false}",
+        "Allow arrays to be statically initialized",
+        boolRef staticInitArrays),
+       (Expert, "static-init-objects", " {none|staticAllocOnly|all}",
+        "Allow objects to be statically initialized",
+        SpaceString
+        (fn s =>
+         staticInitObjects := (case s of
+                                  "none" => NONE
+                                | "staticAllocOnly" => SOME false
+                                | "all" => SOME true
+                                | _ => usage (concat ["invalid -static-init-objects arg: ", s])))),
        (Normal, "stop", " {f|g|o|tc}", "when to stop",
         SpaceString
         (fn s =>

commit 80ffc2c108c07689c3f2a952fe20d00cab5878bf

Fix bug introduced by 4a49d4d88

mlton/backend/ssa2-to-rssa.fun
@@ -1785,7 +1785,7 @@ fun convert (program as S.Program.T {functions, globals, main, ...},
                                                                       ty = ty} of
                                                         NONE => keep ()
                                                       | SOME soe => static soe
-                                                else ()
+                                                else keep ()
                                            end)
                       | S.Exp.PrimApp {args, prim, ...} =>
                            (case Prim.name prim of