786b860a0f6ca1a4ff663f2747e1e70d84ebd220
Matthew Fluet <matthew.fluet@gmail.com>
2020-08-04 19:59:49 -0400

    Simplify `runtime/platform/solaris.{c,h}`
    Solaris 11 is much more standards compliant

--- runtime/platform/solaris.c
+++ runtime/platform/solaris.c
@@ -1,44 +1,11 @@
 #include "platform.h"
 
-#include <ieeefp.h>
-
 #include "platform/diskBack.unix.c"
-#include "platform/float-math.c"
 #include "platform/mmap-protect.c"
 #include "platform/nonwin.c"
 #include "platform/sysconf.c"
 #include "platform/setenv.putenv.c"
 #include "platform/use-mmap.c"
-
-#ifdef __sparc__
-int fegetround (void) {
-        int mode;
-
-        mode = fpgetround ();
-        switch (mode) {
-        case FP_RN: mode = 0; break;
-        case FP_RM: mode = 1; break;
-        case FP_RP: mode = 2; break;
-        case FP_RZ: mode = 3; break;
-        default:
-                die ("fegetround: invalid mode %d\n", mode);
-        }
-        return mode;
-}
-
-int fesetround (int mode) {
-        switch (mode) {
-        case 0: mode = FP_RN; break;
-        case 1: mode = FP_RM; break;
-        case 2: mode = FP_RP; break;
-        case 3: mode = FP_RZ; break;
-        default:
-                die ("fesetround: invalid mode %d\n", mode);
-        }
-        fpsetround (mode);
-        return 0;
-}
-#endif /* __sparc__ */
 
 /* ------------------------------------------------- */
 /*                        GC                         */
