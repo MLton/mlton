MLton/mlton

commit ec2a3afa2bba157819fd59171d3e127360b681ea

Only follow pointers into heap in dfs-mark

runtime/gc/dfs-mark.c
@@ -140,7 +140,7 @@ markInNormal:
     assert (objptrIndex < numObjptrs);
     // next = *(pointer*)todo;
     next = fetchObjptrToPointer (todo, s->heap.start);
-    if (not isPointer (next)) {
+    if (not isPointerInHeap (s, next)) {
 markNextInNormal:
       assert (objptrIndex < numObjptrs);
       objptrIndex++;
@@ -170,7 +170,7 @@ markNextInNormal:
       if (DEBUG_WEAK)
         fprintf (stderr, "marking weak "FMTPTR" ",
                  (uintptr_t)w);
-      if (isObjptr (w->objptr)) {
+      if (isObjptrInHeap (s, w->objptr)) {
         if (DEBUG_WEAK)
           fprintf (stderr, "linking\n");
         w->link = s->weaks;
@@ -216,7 +216,7 @@ markInSequence:
     assert (todo == indexSequenceAtObjptrIndex (s, cur, sequenceIndex, objptrIndex));
     // next = *(pointer*)todo;
     next = fetchObjptrToPointer (todo, s->heap.start);
-    if (not (isPointer(next))) {
+    if (not (isPointerInHeap(s, next))) {
 markNextInSequence:
       assert (sequenceIndex < getSequenceLength (cur));
       assert (objptrIndex < numObjptrs);
@@ -279,7 +279,7 @@ markInFrame:
                "    offset %u  todo "FMTPTR"  next = "FMTPTR"\n",
                frameOffsets [objptrIndex + 1],
                (uintptr_t)todo, (uintptr_t)next);
-    if (not isPointer (next)) {
+    if (not isPointerInHeap (s, next)) {
       objptrIndex++;
       goto markInFrame;
     }

runtime/gc/heap.h
@@ -39,13 +39,11 @@ typedef struct GC_heap {
 
 static inline bool isPointerInOldGen (GC_state s, pointer p);
 static inline bool isPointerInNursery (GC_state s, pointer p);
-#if ASSERT
+static inline bool isPointerInHeap (GC_state s, pointer p);
 static inline bool isObjptrInOldGen (GC_state s, objptr op);
-#endif
 static inline bool isObjptrInNursery (GC_state s, objptr op);
-#if ASSERT
 static inline bool isObjptrInFromSpace (GC_state s, objptr op);
-#endif
+static inline bool isObjptrInHeap (GC_state s, objptr op);
 static inline bool hasHeapBytesFree (GC_state s, size_t oldGen, size_t nursery);
 static inline bool isHeapInit (GC_heap h);
 

runtime/gc/heap_predicates.c
@@ -18,7 +18,6 @@ bool isPointerInNursery (GC_state s, pointer p) {
               and p <= s->frontier));
 }
 
-#if ASSERT
 bool isObjptrInOldGen (GC_state s, objptr op) {
   pointer p;
   if (not (isObjptr(op)))
@@ -26,7 +25,6 @@ bool isObjptrInOldGen (GC_state s, objptr op) {
   p = objptrToPointer (op, s->heap.start);
   return isPointerInOldGen (s, p);
 }
-#endif
 
 bool isObjptrInNursery (GC_state s, objptr op) {
   pointer p;
@@ -36,12 +34,19 @@ bool isObjptrInNursery (GC_state s, objptr op) {
   return isPointerInNursery (s, p);
 }
 
-#if ASSERT
 bool isObjptrInFromSpace (GC_state s, objptr op) {
   return (isObjptrInOldGen (s, op) 
           or isObjptrInNursery (s, op));
 }
-#endif
+
+bool isObjptrInHeap (GC_state s, objptr op) {
+  return isObjptrInFromSpace(s, op);
+}
+
+bool isPointerInHeap (GC_state s, pointer p) {
+  return (isPointerInOldGen (s, p)
+          or isPointerInNursery (s, p));
+}
 
 bool hasHeapBytesFree (GC_state s, size_t oldGen, size_t nursery) {
   size_t total;

commit 47a781c3cd8c242c98466786c695e3ba1f605c00

Fix bug in handling of weak objects during mark-compact GC
During a DFS marking, a weak object should only be linked if its
objptr field is a valid object pointer (and not `BOGUS_OBJPTR`).

This bug was introduced by commit ec2a3afa2, which changed
`is{Objptr,Pointer}` predicates to `is{Objptr,Pointer}InHeap`
predicates.  This was motivated by the introduction of static heaps,
because objects in the static heaps are not traced; in particular,
objects in the immutable static heap are in read-only memory, and
therefore cannot be marked and unmarked.

However, the semantics of these predicates are subtly different.  The
`is{Objptr,Pointer}InHeap` return `TRUE` when the argument is a
non-pointer (i.e., when `is{Objptr,Pointer}` returns `FALSE`).

Commit 2d46cc122 fixed many of the `isPointerInHeap(p)` predicates to
`isPointer(p) and isPointerInHeap(p)`, but missed the `isObjptrInHeap`
that guards the linking of a weak object.

runtime/gc/dfs-mark.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012,2016,2019 Matthew Fluet.
+/* Copyright (C) 2012,2016,2019-2020 Matthew Fluet.
  * Copyright (C) 1999-2007 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  * Copyright (C) 1997-2000 NEC Research Institute.
@@ -174,7 +174,8 @@ markNextInNormal:
       if (DEBUG_WEAK)
         fprintf (stderr, "marking weak "FMTPTR" ",
                  (uintptr_t)w);
-      if (isObjptrInHeap (s, w->objptr)) {
+      if (isObjptr (w->objptr) and
+          isObjptrInHeap (s, w->objptr)) {
         if (DEBUG_WEAK)
           fprintf (stderr, "linking\n");
         w->link = s->weaks;