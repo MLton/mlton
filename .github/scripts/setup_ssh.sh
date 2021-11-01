bb7b2bc65161041fe1e14cf1c67fb9b01a65da19
Matthew Fluet <matthew.fluet@gmail.com>
2021-11-01 15:14:59 -0400

    Remove empty private keys in `setup_ssh.sh` script

--- .github/scripts/setup_ssh.sh
+++ .github/scripts/setup_ssh.sh
@@ -1,5 +1,7 @@
 mkdir -m 0700 -p $HOME/.ssh
 (umask 0077 ; printenv SSH_KEY_RSA >  $HOME/.ssh/id_rsa)
+if [ ! -s $HOME/.ssh/id_rsa ]; then rm -f $HOME/.ssh/id_rsa; fi
 (umask 0077 ; printenv SSH_KEY_ED25519 > $HOME/.ssh/id_ed25519)
+if [ ! -s $HOME/.ssh/id_ed25519 ]; then rm -f $HOME/.ssh/id_ed25519; fi
 (umask 0077 ; printenv SSH_CONFIG > $HOME/.ssh/config)
 (umask 0077 ; printenv SSH_KNOWN_HOSTS > $HOME/.ssh/known_hosts)
