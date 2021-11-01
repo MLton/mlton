9e1b7aea3aef5b8ac4d599c49371068ff69105ea
Matthew Fluet <matthew.fluet@gmail.com>
2021-11-01 15:05:56 -0400

    Use limited account for uploading mlton.sourceforge.io

--- .github/scripts/setup_ssh.sh
+++ .github/scripts/setup_ssh.sh
@@ -1,4 +1,5 @@
 mkdir -m 0700 -p $HOME/.ssh
-(umask 0077 ; printenv SSH_KEY > $HOME/.ssh/id_rsa)
+(umask 0077 ; printenv SSH_KEY_RSA >  $HOME/.ssh/id_rsa)
+(umask 0077 ; printenv SSH_KEY_ED25519 > $HOME/.ssh/id_ed25519)
 (umask 0077 ; printenv SSH_CONFIG > $HOME/.ssh/config)
 (umask 0077 ; printenv SSH_KNOWN_HOSTS > $HOME/.ssh/known_hosts)
