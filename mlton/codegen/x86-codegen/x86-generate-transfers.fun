MLton/mlton

commit 286a54cc0fe80de28cc27c82c371dd6b3c6186dc

Sign- or zero-extend 8- and 16-bit args for C calls in x86 and amd64 codegens
The x86 and x86-64 calling conventions require 8- and 16-bit arguments to be
passed as 32-bit values (on the stack for x86 and in registers for x86-64).

The x86-64 SysV ABI doesn't specify the contents of the upper bits for such
argument extensions.  Both gcc and clang sign- or zero-extend according to the
declared type of the argument.  Furthermore, clang assumes such a type-directed
extension of incoming arguments (and can optimize accordingly).  See
https://stackoverflow.com/questions/36706721/is-a-sign-or-zero-extension-required-when-adding-a-32bit-offset-to-a-pointer-for/36760539#36760539.

For example, consider the function:

    unsigned int s8_to_u32(signed char x) {
        return (unsigned int)x;
    }

clang will optimize to

    movl %edi, %eax

while gcc will optimize to

    movsbl %dil, %eax

For slightly increased clang compatibility, the x86 and amd64 codegens are
modified to emit either a signed or zero extension conditional on the declared
type of the argument in the C function's prototype (rather than unconditionally
using a zero extension).

mlton/codegen/x86-codegen/x86-generate-transfers.fun
@@ -1,4 +1,4 @@
-(* Copyright (C) 2009,2019-2021 Matthew Fluet.
+(* Copyright (C) 2009,2019-2022 Matthew Fluet.
  * Copyright (C) 1999-2008 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  * Copyright (C) 1997-2000 NEC Research Institute.
@@ -1099,6 +1099,7 @@ struct
                      datatype z = datatype CFunction.SymbolScope.t
                      datatype z = datatype CFunction.Target.t
                      val CFunction.T {convention,
+                                      prototype = (protoArgs, _),
                                       return = returnTy,
                                       symbolScope,
                                       target, ...} = func
@@ -1130,9 +1131,9 @@ struct
                                   args)
                               end
                      val (pushArgs, size_args)
-                       = List.fold
-                         (args, (AppendList.empty, 0),
-                          fn ((arg, size), (assembly_args, size_args)) =>
+                       = List.fold2
+                         (args, Vector.toList protoArgs, (AppendList.empty, 0),
+                          fn ((arg, size), protoArg, (assembly_args, size_args)) =>
                           let
                              val (assembly_arg, size_arg) =
                                 if Size.eq (size, Size.DBLE)
@@ -1163,7 +1164,9 @@ struct
                                         orelse Size.eq (size, Size.WORD)
                                    then (AppendList.fromList
                                          [Assembly.instruction_movx
-                                          {oper = Instruction.MOVZX,
+                                          {oper = if CType.isSignedInt protoArg
+                                                     then Instruction.MOVSX
+                                                     else Instruction.MOVZX,
                                            dst = applyFFTempArg,
                                            src = arg,
                                            dstsize = wordSize,

mlton/codegen/amd64-codegen/amd64-generate-transfers.fun
@@ -1,4 +1,4 @@
-(* Copyright (C) 2009,2019-2021 Matthew Fluet.
+(* Copyright (C) 2009,2019-2022 Matthew Fluet.
  * Copyright (C) 1999-2008 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  * Copyright (C) 1997-2000 NEC Research Institute.
@@ -1119,7 +1119,8 @@ struct
                      datatype z = datatype CFunction.Convention.t
                      datatype z = datatype CFunction.SymbolScope.t
                      datatype z = datatype CFunction.Target.t
-                     val CFunction.T {convention=_,
+                     val CFunction.T {convention = _,
+                                      prototype = (protoArgs, _),
                                       return = returnTy,
                                       symbolScope,
                                       target, ...} = func
@@ -1159,28 +1160,29 @@ struct
                      val (setup_args,
                           (reg_args, xmmreg_args),
                           size_stack_args, _)
-                       = List.fold
-                         (args, (AppendList.empty,
-                                 ([],[]),0,
-                                 (if win64
-                                  then [Register.rcx,Register.rdx,
-                                        Register.r8,Register.r9]
-                                  else [Register.rdi,Register.rsi,Register.rdx,
-                                        Register.rcx,Register.r8,Register.r9],
-                                  if win64
-                                  then [(XmmRegister.xmm0D,XmmRegister.xmm0S),
-                                        (XmmRegister.xmm1D,XmmRegister.xmm1S),
-                                        (XmmRegister.xmm2D,XmmRegister.xmm2S),
-                                        (XmmRegister.xmm3D,XmmRegister.xmm3S)]
-                                  else [(XmmRegister.xmm0D,XmmRegister.xmm0S),
-                                        (XmmRegister.xmm1D,XmmRegister.xmm1S),
-                                        (XmmRegister.xmm2D,XmmRegister.xmm2S),
-                                        (XmmRegister.xmm3D,XmmRegister.xmm3S),
-                                        (XmmRegister.xmm4D,XmmRegister.xmm4S),
-                                        (XmmRegister.xmm5D,XmmRegister.xmm5S),
-                                        (XmmRegister.xmm6D,XmmRegister.xmm6S),
-                                        (XmmRegister.xmm7D,XmmRegister.xmm7S)])),
-                          fn ((arg, size), 
+                       = List.fold2
+                         (args, Vector.toList protoArgs,
+                          (AppendList.empty,
+                           ([],[]),0,
+                           (if win64
+                               then [Register.rcx,Register.rdx,
+                                     Register.r8,Register.r9]
+                               else [Register.rdi,Register.rsi,Register.rdx,
+                                     Register.rcx,Register.r8,Register.r9],
+                            if win64
+                               then [(XmmRegister.xmm0D,XmmRegister.xmm0S),
+                                     (XmmRegister.xmm1D,XmmRegister.xmm1S),
+                                     (XmmRegister.xmm2D,XmmRegister.xmm2S),
+                                     (XmmRegister.xmm3D,XmmRegister.xmm3S)]
+                               else [(XmmRegister.xmm0D,XmmRegister.xmm0S),
+                                     (XmmRegister.xmm1D,XmmRegister.xmm1S),
+                                     (XmmRegister.xmm2D,XmmRegister.xmm2S),
+                                     (XmmRegister.xmm3D,XmmRegister.xmm3S),
+                                     (XmmRegister.xmm4D,XmmRegister.xmm4S),
+                                     (XmmRegister.xmm5D,XmmRegister.xmm5S),
+                                     (XmmRegister.xmm6D,XmmRegister.xmm6S),
+                                     (XmmRegister.xmm7D,XmmRegister.xmm7S)])),
+                          fn ((arg, size), protoArg,
                               (setup_args,
                                (reg_args, xmmreg_args),
                                size_stack_args,
@@ -1247,7 +1249,9 @@ struct
                                                    (AppendList.fromList
                                                     [if Size.lt (size, Size.QUAD)
                                                         then Assembly.instruction_movx
-                                                             {oper = Instruction.MOVZX,
+                                                             {oper = if CType.isSignedInt protoArg
+                                                                        then Instruction.MOVSX
+                                                                        else Instruction.MOVZX,
                                                               src = arg,
                                                               dst = Operand.memloc mem,
                                                               srcsize = size,
@@ -1274,7 +1278,9 @@ struct
                                                    size = pointerSize},
                                                   if Size.lt (size, Size.QUAD)
                                                      then Assembly.instruction_movx
-                                                          {oper = Instruction.MOVZX,
+                                                          {oper = if CType.isSignedInt protoArg
+                                                                     then Instruction.MOVSX
+                                                                     else Instruction.MOVZX,
                                                            src = arg,
                                                            dst = c_stackPDerefWord,
                                                            srcsize = size,

commit c53bcb1738bc43eab06605fd76845488fbb1aaf8

Fix bug in X86GenerateTransfers introduced by 286a54cc0.
Commit 286a54cc0 introduced a dependency on the declared prototype of
a C function when generating the assembly for a C function call, in
order to properly sign extend 8-bit and 16-bit arguments.

Early in the x86 codegen, Word64 values are converted into pairs of
Word32 values, so a C function call with Word64 values will converted
to a call with pairs of Word32 values for those arguments (increasing
the number of arguments relative to the earlier IRs).  But, no such
translation was made to the C function's prototype, which continued to
use Int64 and Word64 types.  This lead to a `Fail: fold2`
internal-compiler error due to the mismatch in the number of actual
arguments and the number of prototype arguments.

A simple solution is to expand the Int64 and Word64 prototype
arguments to pairs of Word32 arguments (signedness does not matter).

mlton/codegen/x86-codegen/x86-generate-transfers.fun
@@ -1,4 +1,4 @@
-(* Copyright (C) 2009,2019-2022 Matthew Fluet.
+(* Copyright (C) 2009,2019-2023 Matthew Fluet.
  * Copyright (C) 1999-2008 Henry Cejtin, Matthew Fluet, Suresh
  *    Jagannathan, and Stephen Weeks.
  * Copyright (C) 1997-2000 NEC Research Institute.
@@ -1130,9 +1130,16 @@ struct
                                     size = #2 fptrArg}),
                                   args)
                               end
+                     val protoArgs =
+                        Vector.foldr
+                        (protoArgs, [], fn (protoArg, protoArgs) =>
+                         case protoArg of
+                            CType.Int64 => CType.Word32 :: CType.Word32 :: protoArgs
+                          | CType.Word64 => CType.Word32 :: CType.Word32 :: protoArgs
+                          | _ => protoArg :: protoArgs)
                      val (pushArgs, size_args)
                        = List.fold2
-                         (args, Vector.toList protoArgs, (AppendList.empty, 0),
+                         (args, protoArgs, (AppendList.empty, 0),
                           fn ((arg, size), protoArg, (assembly_args, size_args)) =>
                           let
                              val (assembly_arg, size_arg) =