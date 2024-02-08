0107aa5e2227b952f70c8ead5c355c21ef955315
Adam Goode <agoode@google.com>
2024-02-07 21:51:48 -0500

    Fix up some compiler warnings related to prototypes

--- runtime/gc/gc_state.h
+++ runtime/gc/gc_state.h
@@ -113,4 +113,4 @@ PRIVATE void GC_setGCSignalHandled (GC_state s, Bool_t b);
 PRIVATE Bool_t GC_getGCSignalPending (GC_state s);
 PRIVATE void GC_setGCSignalPending (GC_state s, Bool_t b);
 
-PRIVATE GC_state MLton_gcState ();
+PRIVATE GC_state MLton_gcState (void);
--- runtime/gc/profiling.h
+++ runtime/gc/profiling.h
@@ -107,7 +107,7 @@ PRIVATE GC_profileData profileMalloc (GC_state s);
 PRIVATE void profileWrite (GC_state s, GC_profileData p, const char* fileName);
 PRIVATE void profileFree (GC_state s, GC_profileData p);
 
-static void GC_handleSigProf ();
+static void GC_handleSigProf (int signum);
 static void setProfTimer (suseconds_t usec);
 static void initProfilingTime (GC_state s);
 static void atexitForProfiling (void);
